b09effe915835e2532dc4ce81ad3682593d385d7
David Drurika <tralandia.david@gmail.com>
2012-03-08 14:10:55 +0100

    fix pripojenia k DB

--- app/extras/Doctrine/TablePrefix.php
+++ app/extras/Doctrine/TablePrefix.php
@@ -4,7 +4,8 @@ use \Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 
 class TablePrefix
 {
-    protected $prefix = 'pair_';
+    protected $prefix = '';
+    protected static $c = 0;
 
     public function __construct($prefix) {
         $this->prefix = (string) $prefix;
@@ -14,10 +15,12 @@ class TablePrefix
         $classMetadata = $eventArgs->getClassMetadata();
         $classMetadata->setTableName($this->prefix . $classMetadata->getTableName());
         foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
-            if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+            //if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+                if(!isset($classMetadata->associationMappings[$fieldName]['joinTable'])) continue;
+                if(!isset($classMetadata->associationMappings[$fieldName]['joinTable']['name'])) continue;
                 $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
-                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
-            }
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . self::$c++ . $mappedTableName;
+            //}
         }
     }
 
