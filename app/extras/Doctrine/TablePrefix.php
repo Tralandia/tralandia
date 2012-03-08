722408bcdd6c2f003a4e517b3efc46b8db92979d
David Drurika <tralandia.david@gmail.com>
2012-03-08 14:50:05 +0100

    prefix update

--- app/extras/Doctrine/TablePrefix.php
+++ app/extras/Doctrine/TablePrefix.php
@@ -5,23 +5,23 @@ use \Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 class TablePrefix
 {
     protected $prefixAll = NULL;
-    protected $prefixMtM = NULL;
+    protected $prefixJoinTables = NULL;
     protected static $c = 0;
 
-    public function __construct($prefixAll, $prefixMtM) {
+    public function __construct($prefixAll, $prefixJoinTables) {
         $this->prefixAll = $prefixAll;
-        $this->prefixMtM = $prefixMtM;
+        $this->prefixJoinTables = $prefixJoinTables;
     }
 
     public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs) {
         $classMetadata = $eventArgs->getClassMetadata();
         if(isset($this->prefixAll)) $classMetadata->setTableName($this->prefixAll . $classMetadata->getTableName());
         foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
-            if (isset($this->prefixMtM) && $mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
+            if ((isset($this->prefixJoinTables) || isset($this->prefixAll)) && $mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
                 if(!isset($classMetadata->associationMappings[$fieldName]['joinTable'])) continue;
                 if(!isset($classMetadata->associationMappings[$fieldName]['joinTable']['name'])) continue;
                 $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
-                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefixMtM . $mappedTableName;
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefixAll . $this->prefixJoinTables . $mappedTableName;
             }
         }
     }
