91559f9df25e8911d8638f46930d03c587497c99
Cibi <tralandia.cibi@gmail.com>
2012-03-14 16:49:47 +0100

    Currency test

--- app/AdminModule/presenters/CibiPresenter.php
+++ app/AdminModule/presenters/CibiPresenter.php
@@ -7,20 +7,30 @@ use \Services as S;
 class CibiPresenter extends BasePresenter {
 
 	public function actionDefault() {
+		
+		//$this->Currency();
+
+	}
+
+	public function renderDefault() {
+
+	}
+
+	private function Currency() {
+
+		$c = new S\CurrencyService(2);
+		$c->setIso(33)->save();
+		debug($c);
+		
 		$currency = new S\CurrencyService;
 		$currency
 			->setIso(66)
 			->setExchangeRate(56.0)
 			->setDecimalPlaces(2)
-			->setRounding(2)
-			->setCreated()
-			->setUpdated();
+			->setRounding(2);
 		
 		debug($currency);
 		$currency->save();
-	}
-
-	public function renderDefault() {
 
 	}
 
