e214d0f70e9a1c8e1c61e91755a6712ca4fefada
Branislav Vaculčiak <vaculciak@softone.sk>
2012-03-20 18:41:08 +0100

    presun kodu BaseService -> Service, priprava api

--- app/extras/Models/Service.php
+++ app/extras/Models/Service.php
@@ -1,51 +1,131 @@
 <?php
 
-namespace Extras\Models;
+namespace Tra\Services;
 
 use Nette, 
-	Extras\Entity, 
+	Tra, 
+	Entity, 
 	Nette\ObjectMixin, 
 	Nette\MemberAccessException,
-	Doctrine\ORM\EntityManager;
+	Doctrine\ORM\EntityManager,
+	Tra\Services\ServiceLoader;
 
+/**
+ * Abstrakcia mocnej vrstvy sluzba
+ */
 abstract class Service extends Nette\Object implements IService {
 	
+	/**
+	 * @var string
+	 */
 	const MAIN_ENTITY_NAME = null;
+	
+	/**
+	 * @var Entity
+	 */
 	protected $mainEntity = false;
+
+	/**
+	 * @var Reflector
+	 */
 	protected $reflector = null;
+
+	/**
+	 * @var EntityManager
+	 */
 	private static $em = null;
+
+	/**
+	 * @var bool
+	 */
 	private static $flush = true;
 
-	public function __construct() {
+	/**
+	 * @var bool
+	 */
+	private $isPersist = false;
 
+	/**
+	 * @param bool
+	 */
+	public function __construct($new = true) {
+		if ($this->getEntityManager() === null) {
+			throw new \Exception("Nie je dostupny EntityManager");
+		}
+		if ($new) {
+			$entityName = $this->getMainEntityName();
+			$this->mainEntity = new $entityName;
+		}
 	}
 
+	/**
+	 * Nastavenie entity manazera
+	 * @param EntityManager
+	 */
 	public static function setEntityManager(EntityManager $em) {
 		self::$em = $em;
 	}
 
+	/**
+	 * Pozastavi okamzite posielanie sql dotazov
+	 */
 	public static function preventFlush() {
 		self::$flush = false;
 	}
 	
+	/**
+	 * Vykona odoslanie pozastavenych sql dotazov
+	 */
 	public static function flush() {
 		self::$em->flush();
 		self::$flush = true;
 	}
 	
+	/**
+	 * Je pozielanie sql dotazov povolene?
+	 * @return bool
+	 */
 	public static function isFlushable() {
 		return self::$flush;
 	}
+
+	/**
+	 * Ziska jedinecnu instanciu servisy
+	 * @param integer
+	 * @return IService
+	 */
+	public static function get($id) {
+		$key = get_called_class() . '#' . $id;
+
+		if (ServiceLoader::exists($key)) {
+			return ServiceLoader::get($key);
+		}
+		$service = new static(false);
+		$service->load($id);
+		ServiceLoader::set($key, $service);
+		return $service;
+	}
 	
+	/**
+	 * Setter
+	 * @param string
+	 * @param mixed
+	 */
 	public function __set($name, $value) {
 		if ($value instanceof Service) {
-			return $this->mainEntity->$name = $value->getMainEntity();
+			$this->mainEntity->$name = $value->getMainEntity();
 		}
 		if ($this->mainEntity instanceof Entity) {
-			return $this->mainEntity->$name = $value;
+			$this->mainEntity->$name = $value;
 		}
 	}
 
+	/**
+	 * Getter
+	 * @param string
+	 * @param mixed
+	 * @return mixed
+	 */
 	public function &__get($name) {
 		if ($this->mainEntity instanceof Entity) {
 			try {
@@ -56,6 +136,11 @@ abstract class Service extends Nette\Object implements IService {
 		return ObjectMixin::get($this, $name);
 	}
 
+	/**
+	 * Magia
+	 * @param string
+	 * @param mixed
+	 */
 	public function __call($name, $arguments) {
 		try {
 			if($this->mainEntity instanceof Entity) {
@@ -76,6 +161,10 @@ abstract class Service extends Nette\Object implements IService {
 		} catch (MemberAccessException $e) {}
 	}
 
+	/**
+	 * Ziskanie hlavnej entity
+	 * @return Entity
+	 */
 	public function getMainEntity() {
 		if (!$this->mainEntity) {
 			throw new \Exception("Este nebola zadana `mainEntity`");
@@ -84,6 +173,10 @@ abstract class Service extends Nette\Object implements IService {
 		return $this->mainEntity;
 	}
 
+	/**
+	 * Ziskanie nazvu hlavnej entity
+	 * @return string
+	 */
 	public function getMainEntityName() {
 		if (!static::MAIN_ENTITY_NAME) {
 			throw new \Exception("Este nebola zadana `mainEntity`, preto nemozem ziskat jej nazov");
@@ -91,7 +184,11 @@ abstract class Service extends Nette\Object implements IService {
 		
 		return static::MAIN_ENTITY_NAME;
 	}
-	
+
+	/**
+	 * Ziskanie reflektora
+	 * @return Reflector
+	 */
 	public function getReflector() {
 		if ($this->reflector === null) {
 			$this->reflector = new Reflector($this);
@@ -99,14 +196,87 @@ abstract class Service extends Nette\Object implements IService {
 		return $this->reflector;
 	}
 
+	/**
+	 * Ziskanie entity manazera
+	 * @return EntityManager
+	 */
 	protected function getEntityManager() {
 		return self::$em;
 	}
 
+	/**
+	 * Alias na entity manazera
+	 * @return EntityManager
+	 */
 	protected function getEm() {
 		return self::getEntityManager();
 	}
-	
+
+	/**
+	 * Ziskanie zoznamu
+	 * @return array
+	 */
+	public function getList($class, $key, $value) {
+		return $this->getEm()->getRepository($class)->fetchPairs($key, $value);
+	}
+
+	/**
+	 * Ziskanie datasource
+	 * @return Query
+	 */
+	public function getDataSource() {
+		$query = $this->getEm()->createQueryBuilder();
+		$query->select('e')->from($this->getMainEntityName(), 'e');
+		return $query;
+	}
+
+	/**
+	 * Ziskanie hlavnej entity z entity manazera
+	 * @param integer
+	 */
+	protected function load($id) {
+		if ($entity = $this->getEm()->find($this->getMainEntityName(), $id)) {
+			$this->isPersist = true;
+			$this->mainEntity = $entity;
+		}
+	}
+
+	/**
+	 * Ulozenie zmien (vytvorenie/aktualizovanie)
+	 */
+	public function save() {
+		try {
+			if ($this->mainEntity instanceof Entity) {
+				if (!$this->isPersist) {
+					$this->getEm()->persist($this->mainEntity);
+				}
+				if ($this->isFlushable()) {
+					$this->getEm()->flush();
+				}
+			}
+		} catch (\PDOException $e) {
+			throw new \Tra\Services\ServiceException($e->getMessage());
+		}
+	}
+
+	/**
+	 * Zmazanie hlavnej entity
+	 */
+	public function delete() {
+		try {
+			if ($this->mainEntity instanceof Entity) {
+				$this->getEm()->remove($this->mainEntity);
+				//TODO: netreba sa pytat tiez na $this->isFlushable() ?
+				$this->getEm()->flush();
+			}
+		} catch (\PDOException $e) {
+			throw new \Tra\Services\ServiceException($e->getMessage());
+		}
+	}
+
+	/**
+	 * Pripravi data
+	 */
 	public function prepareData(Tra\Forms\Form $form) {
 		$assocations = $this->getReflector()->getAssocations();
 		$values = $form->getValues();
@@ -121,6 +291,9 @@ abstract class Service extends Nette\Object implements IService {
 		return $values;
 	}
 	
+	/**
+	 * Pripravi data pre grid
+	 */
 	public function prepareGridData(Tra\Forms\Form $form) {
 		$assocations = $this->getReflector()->getAssocations();
 		$values = $form->getValues();
