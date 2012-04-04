4f513d9a8b40a2fb9cb46d7428f182d1003e88ba
David Drurika <tralandia.david@gmail.com>
2012-04-04 21:03:24 +0200

    save

--- app/extras/MyMacros.php
+++ app/extras/MyMacros.php
@@ -3,19 +3,9 @@ namespace Extras;
 
 class MyMacros extends \Nette\Latte\Macros\MacroSet {
 
-        public static function install(\Nette\Latte\Compiler $compiler) {
-        	$me = parent::install($compiler);
-            //$me->addMacro('ulList', array($me, 'macroUlList'), '</ul>');
-            $me->addMacro('ulList', 'echo \'src="\' . $template->dataStream(%node.word) . \'"\'');
-            $me->addMacro('ulList', 'echo \Extras\Component\UlList(%node.args)');
-            return $me;
-        }
-
-        public function macroUlList(\Nette\Latte\MacroNode $node, \Nette\Latte\PhpWriter $writer) {
-            debug($node, $writer);
-            $node->openingCode = '<ul>';
-            $node->closingCode = '</ul>';
-            debug($writer->write('%node.word'));
-        }
-
+		public static function install(\Nette\Latte\Compiler $compiler) {
+			$me = parent::install($compiler);
+			$me->addMacro('ulList', 'echo \Extras\Component\UlList(%node.args)');
+			return $me;
+		}
 }
