6e3b94b98f6bc031830158c28087bd233f1872f6
Cibi <tralandia.cibi@gmail.com>
2012-04-06 09:46:18 +0200

    ulList macro

--- app/FrontModule/presenters/BasePresenter.php
+++ app/FrontModule/presenters/BasePresenter.php
@@ -17,10 +17,7 @@ abstract class BasePresenter extends \BasePresenter {
 
 		$this->template->supportedLanguages = \Service\Dictionary\LanguageList::getBySupported(\Entity\Dictionary\Language::SUPPORTED);
 		$this->template->launchedCountries = \Service\Location\CountryList::getByStatus(\Entity\Location\Country::STATUS_LAUNCHED, null, 5);
-		debug($this->template->launchedCountries);
 		$this->template->liveRentalsCount = count(\Service\Rental\RentalList::getByStatus(\Entity\Rental\Rental::STATUS_LIVE));
-		
-		//debug($this->environment->getCountry());
 
 		/******* Things TODO *****/
 		$this->template->mainMenuItems = $this->getMainMenuItems();
