dd2f9d6a9b14a823a3bdc728cf35634aa6c13078
David Drurika <tralandia.david@gmail.com>
2012-04-16 14:46:40 +0200

    UlList update

--- app/BaseModule/presenters/BasePresenter.php
+++ app/BaseModule/presenters/BasePresenter.php
@@ -14,6 +14,12 @@ abstract class BasePresenter extends Presenter {
 		$this->template->registerHelper('image', callback('Tools::helperImage'));
 	}
 
+	protected function createTemplate($class = NULL) {
+		$template = parent::createTemplate($class);
+		$template->registerHelper('ulList', callback($this, 'ulListHelper'));
+		return $template;
+	}
+
 	protected function createComponentFlashes($name) {
 		return new FlashesControl($this, $name);
 	}
@@ -101,6 +107,59 @@ abstract class BasePresenter extends Presenter {
 
 	public function getBaseUrl() {
 		return $this->template->baseUrl;
+	}
+
+	/* --------------------- Helpers --------------------- */
+
+	public function ulListHelper($data, $columnCount = 3, $li = NULL) {
+		if(!($data instanceof \Traversable || is_array($data))) {
+			throw new \Nette\InvalidArgumentException('Argument "$data" does not match with the expected value');
+		}
+
+		if(!is_numeric($columnCount) || $columnCount <= 0) {
+			throw new \Nette\InvalidArgumentException('Argument "$columnCount" does not match with the expected value');
+		}
+		// @cibi ked chces nieco prelozit tak to zapis takto:
+		// $text = $this->template->translate(123);
+		if($li === NULL) {
+			$li = '<li>%name% - {_123}</li>';
+		}
+
+		preg_match_all('/%[a-zA-Z\.]+%/', $li, $matches);
+
+		$replaces = array();
+		foreach ($matches[0] as $match) {
+			if (gettype($data)=='object') {
+				$value = '$item->'.str_replace('.', '->', substr($match, 1, -1));
+			} else {
+				$value = '$item["'.str_replace('.', '"]["', substr($match, 1, -1)).'"]';
+			}
+			$replaces[$match] = $value;
+		}
+
+		$newData = array();
+		for ($i=0; $i < $columnCount; $i++) {
+			foreach ($data as $k=>$item) {
+				$search = array();
+				$replace = array();
+				foreach ($replaces as $key => $value) {
+					$search[] = $key;
+					eval('$r = '.$value.';');
+					$replace = $r;
+				}
+				$liTemp = str_replace($search, $replace, $li);
+				$newData[$i][] = $liTemp;
+				unset($data[$k]);
+				break;
+			}
+		}
+
+		$return = array();
+		foreach ($newData as $key => $value) {
+			$return[] = '<ul>'.implode('', $value).'</ul>';
+		}
+
+		return implode('', $return);
 	}
 
 }
