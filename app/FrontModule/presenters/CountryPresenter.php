de738005db35a56949451d08da0a4eda59053867
Cibi <tralandia.cibi@gmail.com>
2012-04-23 09:13:40 +0200

    Country presenter

--- app/FrontModule/presenters/CountryPresenter.php
+++ app/FrontModule/presenters/CountryPresenter.php
@@ -6,50 +6,26 @@ class CountryPresenter extends BasePresenter {
 	
 	public function actionList() {
 
-debug('ddd');
+		$continentsType = \Service\Location\Type::getBySlug('continent');
+		$countriesType = \Service\Location\Type::getBySlug('country');
+
+		$continents = \Nette\ArrayHash::from(array());
+		foreach (\Service\Location\LocationList::getByType($continentsType) as $key=>$continent) {
+			$countries[$key] = \Nette\ArrayHash::from(array());
+			foreach (\Service\Location\LocationList::getBy(array('type'=>$countriesType, 'parentId'=>$continent)) as $key2=>$country) {
+				$countries[$key][$key2] = $country;
+			}
+			if (count($countries[$key])) $continents[$key] = $continent;
+		}
+
+		$this->template->continents = $continents;
+		$this->template->countries = $countries;
 
 	}
 
 	public function renderDefault() {
 
-		$vp = new \VisualPaginator($this, 'vp');
-		$vp->templateFile = APP_DIR.'/FrontModule/components/VisualPaginator/paginator.latte';
-
-		$paginator = $vp->getPaginator();
-		$paginator->itemsPerPage = 15;
-		$paginator->itemCount = 568;
-
-	}
-
-	public function createComponentCountryMap($name) {
-
-		return new \FrontModule\Components\CountryMap\CountryMap($this, $name);
-
-	}
-
-	public function createComponentTabControl($name) {
-
-		$tabBar = new \BaseModule\Components\TabControl\TabControl($this, $name);
-
-		$t = $tabBar->addTab('top');
-		$t->setHeader(806)->setContent('Top Objekty'); // @PHRASE: {_806, '1955 TOP holiday homes'}
-
-		$t = $tabBar->addTab('regions');
-		$content = new \FrontModule\Components\RegionsPage\Regions($this, 'RegionsPage');
-		$t->setHeader(678)->setContent($content)->setActive(); // @PHRASE: {_678, '1112 Regions'}
-
-		$t = $tabBar->addTab('localities');
-		$content = new \FrontModule\Components\LocalitiesPage\Localities($this, 'LocalitiesPage');
-		$t->setHeader(725)->setContent($content); // @PHRASE: {_725, '1626 Towns / Villages'}
-
-		$t = $tabBar->addTab('tags');
-		$content = new \FrontModule\Components\TagsPage\Tags($this, 'TagsPage');
-		$t->setHeader(727)->setContent($content); // @PHRASE: {_727, '1628 Types of stays'}
-
-		$t = $tabBar->addTab('about');
-		$content = new \FrontModule\Components\GenericPage\Generic($this, 'GenericPage');
-		$content->slug = 'about';
-		$t->setHeader(1163)->setContent($content); // @PHRASE: {_1163, '23011 '}
+		
 
 	}
 
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -81,7 +81,7 @@ $container->application->onStartup[] = function() use ($container) {
 	));
 
 	$router[] = $frontRouter = new RouteList('Front');
-	$frontRouter[] = new \Extras\Route('Home:default');
+	// $frontRouter[] = new \Extras\Route('Home:default');
 	$frontRouter[] = new Route('<presenter>/[<action>[/<id>]]', 'Home:default');
 
 };
