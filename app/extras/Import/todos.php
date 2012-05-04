0f11a8357c3d99ea2ed60e0f9a1240d601fb7607
Radoslav Toth <toth.radoslav@gmail.com>
2012-05-04 12:51:06 +0200

    a

--- app/extras/Import/todos.php
+++ app/extras/Import/todos.php
@@ -10,8 +10,5 @@ Rental - este dorobit:
 
 
 Current status:
-- rental sa spusta na prvych 20-tich kusoch, iba 3 fotky
-- pre User som pridal atribut isOwner
-- dal som undo pre Users a nejde to, su tam zle vztahy, je to v FF
-- problem s chybajucimi indexami asi v phpmyadmin vidiet
+- 
 
