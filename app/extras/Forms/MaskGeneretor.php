ec3f834364f8ab0d01f730c944a488de5e51475c
David Drurika <tralandia.david@gmail.com>
2012-10-02 08:58:23 +0200

    save

--- app/extras/Forms/MaskGeneretor.php
+++ app/extras/Forms/MaskGeneretor.php
@@ -4,6 +4,7 @@ namespace Extras\Forms;
 
 use Nette;
 use Extras;
+use Nette\Utils\Arrays;
 
 class MaskGenerator extends \Nette\Object {
 
@@ -18,8 +19,10 @@ class MaskGenerator extends \Nette\Object {
 
 	public function fillMask(Mask $mask) {
 		$fields = $this->config['form']['fields'];
+		d($fields);
 		foreach ($fields as $fieldName => $field) {
-			d($field);
+			$type = $this->getControlType($field);
+			//$mask->add(constant())
 		}
 
 		return $mask;
@@ -29,6 +32,14 @@ class MaskGenerator extends \Nette\Object {
 		$generator = new static($entityReflection, $config);
 		$mask = $generator->fillMask($mask);
 		return $mask;
+	}
+
+	protected function getControlType($field) {
+		$type = Arrays::get($field, array('control', 'type'), NULL);
+		if(!$type) {
+			throw new \Nette\InvalidArgumentException('Pre ' . $field['label'] . ' si nenastavil "control:type"');
+		}
+		return $type;
 	}
 
 }
