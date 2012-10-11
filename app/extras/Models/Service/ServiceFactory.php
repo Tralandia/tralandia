6bdd5da5630befad6413e95b2b1981e69221d446
David Drurika <tralandia.david@gmail.com>
2012-10-11 12:20:05 +0200

    Uprevene serviceFactory + zmazane ModelFactory

--- app/extras/Models/Service/ServiceFactory.php
+++ app/extras/Models/Service/ServiceFactory.php
@@ -1,6 +1,9 @@
 <?php
 namespace Extras\Models\Service;
 
+use Doctrine\ORM\EntityManager;
+use Extras\Models\Entity\EntityFactory;
+
 /**
  * ServiceFactory class
  *
@@ -10,15 +13,21 @@ class ServiceFactory extends \Nette\Object {
 
 	public $model;
 	public $serviceName;
+	public $entityFactory;
 
-	public function __construct(\Doctrine\ORM\EntityManager $model, $serviceName) {
+	public function __construct(\Doctrine\ORM\EntityManager $model, $serviceName, EntityFactory $entityFactory) {
 		$this->model = $model;
 		$this->serviceName = $serviceName;
+		$this->entityFactory = $entityFactory;
 	}
 
-	public function create($entity) {
+	public function create($entity = NULL) {
 		$serviceName = $this->serviceName;
-		return new $serviceName($this->model, $entity);
+		if($entity === NULL && !func_num_args()) {
+			return new $serviceName($this->model, $this->entityFactory->create());
+		} else {
+			return new $serviceName($this->model, $entity);
+		}
 	}
 
 }
