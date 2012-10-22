3a01a4c25838b2d04c0a83adff1385e2b1efb716
Branislav Vaculčiak <vaculciak@softone.sk>
2012-10-22 16:56:58 +0200

    save

--- app/AdminModule/presenters/WaccoPresenter.php
+++ app/AdminModule/presenters/WaccoPresenter.php
@@ -12,13 +12,31 @@ class WaccoPresenter extends BasePresenter {
 	}
 
 	public function actionList() {
-		//$locationLocationRepo = $this->context->model->getRepository('Entity\Location\Location');
-		//$locationTypeRepo = $this->context->model->getRepository('Entity\Location\Type');
-		//$entity = $locationLocationRepo->find(2);
-
-		
 		$repo = $this->context->model->getRepository('Entity\Currency');
 		$entity = $repo->find(2);
+
+		$model = $this->context->model;
+		$configurator = new Extras\Config\Configurator($this->context->params['settingsDir'] . '/presenters/currency.neon');
+
+		$mask = new Extras\FormMask\Mask;
+		$form = new Nette\Application\UI\Form($this, 'form');
+		$form->onSuccess[] = array($mask, 'process');
+		$form->onSuccess[] = function($form) use ($model) {
+			$model->flush();
+		};
+
+		$generator = new Extras\FormMask\Generator($mask, $configurator, $entity);
+		$generator->setItemPhrase($this->context->itemPhraseFactory);
+		$generator->setItemText($this->context->itemTextFactory);
+		$generator->build();
+
+		$mask->extend($form);
+		$this->template->form = $form;
+	}
+
+	public function actionList2() {
+		$repo = $this->context->model->getRepository('Entity\Language');
+		$entity = $repo->find(144);
 
 		$model = $this->context->model;
 		$configurator = new Extras\Config\Configurator($this->context->params['settingsDir'] . '/presenters/currency.neon');
