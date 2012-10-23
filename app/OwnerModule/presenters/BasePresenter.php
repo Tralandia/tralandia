eea521a0df7e157ae4eabbac4b5bd8dce99c8fbb
David Drurika <tralandia.david@gmail.com>
2012-10-23 11:45:32 +0200

    pridany OwnerModul

---
+++ app/OwnerModule/presenters/BasePresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FrontModule;
+
+abstract class BasePresenter extends \BasePresenter {
+	
+	protected function startup() {
+		parent::startup();
+	}
+
+
+}
---
+++ app/OwnerModule/presenters/RentalPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FrontModule;
+
+class RentalPresenter extends BasePresenter {
+
+	protected $rentalRepository;
+
+	protected $rental;
+
+	public function setContext(\Nette\DI\Container $dic) {
+		$this->setProperty('rentalRepository');
+	}
+
+	public function actionEdit($id) {
+
+	}
+
+}
