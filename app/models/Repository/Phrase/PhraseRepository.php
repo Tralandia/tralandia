251d94204b8fd37227020074d1a6078337cfe708
David Drurika <tralandia.david@gmail.com>
2012-10-24 09:41:51 +0200

    save

--- app/models/Repository/Phrase/PhraseRepository.php
+++ app/models/Repository/Phrase/PhraseRepository.php
@@ -12,15 +12,19 @@ class PhraseRepository extends \Repository\BaseRepository {
 
 	public function findMissingTranslations() {
 		$qb = $this->_em->createQueryBuilder();
-		$qb->select('p.id AS pId, count(p.id) AS c')
+		$qb->select('p.id AS pId')
 			->from('\Entity\Phrase\Phrase', 'p')
 			->leftJoin('p.translations', 't')
-			->join('t.language', 'l')
 			->where($qb->expr()->isNull('t.id'))
-			->andWhere('l.supported = 1')
-			->groupBy('p.id');
+			->andWhere('t.language = 1');
 
 		return $qb->getQuery()->getResult();
 	}
 
 }
+
+// select * from phrase where id not in (
+// SELECT p0_.id
+// FROM phrase p0_ 
+// LEFT JOIN phrase_translation p1_ ON p0_.id = p1_.phrase_id 
+// where p1_.language_id = 38)
