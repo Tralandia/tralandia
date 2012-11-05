68afd276820dfe47ba6c4059ffed47bdc2cd88f1
David Drurika <tralandia.david@gmail.com>
2012-11-05 12:46:46 +0100

    save

--- app/extras/Forms/Controls/AdvancedNeon.php
+++ app/extras/Forms/Controls/AdvancedNeon.php
@@ -24,7 +24,7 @@ class AdvancedNeon extends TextBase {
 
 	public function setValue($value) {
 		if(is_array($value)) {
-			$this->value = Neon::encode($value);
+			$this->value = Neon::encode($value, Neon::BLOCK);
 		} else {
 			$this->value = $value;
 		}
