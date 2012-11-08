d5fdb263e114b9115f0c497e888491de6e8c30b0
Branislav Vaculčiak <vaculciak@softone.sk>
2012-11-08 16:30:17 +0100

    takmer hotof

--- app/AdminModule/presenters/WaccoPresenter.php
+++ app/AdminModule/presenters/WaccoPresenter.php
@@ -14,18 +14,28 @@ class WaccoPresenter extends BasePresenter {
 	public function actionList() {
 		$repo = $this->context->model->getRepository('Entity\Currency');
 		$entity = $repo->find(2);
-		$this->template->form = $this->context->presenter->currency->form->create($entity);
+		$this->template->form = $this->getForm('currency', $entity);
 	}
 
 	public function actionList2() {
 		$repo = $this->context->model->getRepository('Entity\Language');
 		$entity = $repo->find(144);
-		$this->template->form = $this->context->presenter->language->form->create($entity);
+		$this->template->form = $this->getForm('language', $entity);
 	}
 
 	public function actionList3() {
 		$repo = $this->context->model->getRepository('Entity\Location\Location');
 		$entity = $repo->find(563);
-		$this->template->form = $this->context->presenter->location->form->create($entity);
+		$this->template->form = $this->getForm('location', $entity);
+	}
+
+	public function getForm($name, $entity) {
+		$model = $this->context->model;
+		$form = $this->context->presenter->{$name}->form->create($entity);
+		$form->onSuccess[] = function($form) use ($model) {
+			$model->flush();
+		};
+		$this->addComponent($form, $name);
+		return $form;
 	}
 }
--- app/extras/FormMask/FormFactory.php
+++ app/extras/FormMask/FormFactory.php
@@ -26,6 +26,7 @@ class FormFactory {
 	 */
 	public function create(Extras\Models\Entity\IEntity $entity) {
 		$form = new Nette\Application\UI\Form;
+		$form->onSuccess[] = array($this->generator->getMask(), 'process');
 		$this->generator->setEntity($entity)->build();
 		$this->generator->getMask()->extend($form);
 		return $form;
