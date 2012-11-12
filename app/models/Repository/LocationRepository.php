e8375221b1840405fa388115dad1657d3cd73077
Branislav Vaculčiak <vaculciak@softone.sk>
2012-11-12 16:23:01 +0100

    vylepsenie repo get items

--- app/models/Repository/LocationRepository.php
+++ app/models/Repository/LocationRepository.php
@@ -10,7 +10,7 @@ class LocationRepository extends BaseRepository {
 
 	public function getItems() {
 		$query = $this->_em->createQueryBuilder();
-		$query->select('e')->from($this->_entityName, 'e');
+		$query->select('e, p')->from($this->_entityName, 'e')->leftJoin('e.name', 'p');
 		return $query->getQuery()->getResult();
 	}
 }
