526b3a641994cc2e3b28dc0a480ce0e907c99600
Radoslav Toth <radoslavtoth@Radoslavs-MacBook-Pro.local>
2012-11-13 16:33:46 +0100

    s

--- app/extras/Import/BaseLanguagesImport.php
+++ app/extras/Import/BaseLanguagesImport.php
@@ -6,6 +6,34 @@ abstract class BaseLanguagesImport extends BaseImport {
 
 	public $languageOptions = array(
 		'sk' => array(
+			'genders' => array(
+				0 => 'Neuter'
+				1 => 'Masculine', 
+				2 => 'Feminine', 
+			),
+			'primaryGender' => 1,
+			'plurals' => array(
+				'5plus' => array('name' => '0, 5+', 'pattern' => '$i==0 || $i>4'),
+				'one' => array('name' => '1', 'pattern' => '$i==1'),
+				'twofour' => array('name' => '2-4', 'pattern' => '$i>1 && $i<5'),
+			),
+			'primarySingular' => 'one',
+			'primaryPlural' => 'twofour',
+		),
+		'en' => array(
+			'genders' => array(),
+			'primaryGender' => NULL,
+			'plurals' => array(
+				'rule' => '(n != 1)',
+				'names' => array(
+					0 => 'Singular',
+					1 => 'Zero, 2 or more',
+				),
+			),
+			'primarySingular' => 0,
+			'primaryPlural' => 1,
+		),
+		'de' => array(
 			'genders' => array('feminine' => 'Feminine', 'masculine' => 'Masculine', 'neuter' => 'Neuter'),
 			'primaryGender' => 'masculine',
 			'plurals' => array(
@@ -16,7 +44,6 @@ abstract class BaseLanguagesImport extends BaseImport {
 			'primarySingular' => 'one',
 			'primaryPlural' => 'twofour',
 		),
-		// 'en' => array(...),
 	);
 
 }
