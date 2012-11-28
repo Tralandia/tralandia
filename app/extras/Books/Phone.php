06f4b4e2d8e1655918b790fa48ed0662af83091d
Branislav Vaculčiak <vaculciak@softone.sk>
2012-11-28 15:56:28 +0100

    save

---
+++ app/extras/Books/Phone.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Extras\Books;
+
+use Nette, Extras, Entity;
+
+/**
+ * Treda sluzi ako databaza unikatnych telefonnych cisel.
+ */
+class Phone extends Nette\Object {
+
+	/** @var Extras\Models\Repository\RepositoryAccessor */
+	private $phoneRepository;
+
+	/** @var string */
+	private $serviceUrl = 'http://tra-devel.soft1.sk:8080/phonenumberparser?';
+
+	/**
+	 * @param Extras\Models\Repository\RepositoryAccessor $phoneRepository
+	 */
+	public function __construct(Extras\Models\Repository\RepositoryAccessor $phoneRepository) {
+		$this->phoneRepository = $phoneRepository;
+	}
+
+	/**
+	 * Vyhlada cislo v DB
+	 * - cislo musi byt v medzinarodnom formate
+	 * @param string $number
+	 * @return Entity\Contacts\Phone|false
+	 */
+	public function find($number) {
+		return $this->phoneRepository->get()->findOneByPhone($this->prepareNumber($number));
+	}
+
+	/**
+	 * Skusi najst cislo, ak nenajde vytvori nove a vrati jeho zaznam
+	 * @param string $number
+	 * @return Entity\Contacts\Phone
+	 */
+	public function getOrCreate($number) {
+		if (!$phone = $this->find($number)) {
+			$response = $this->serviceRequest($number);
+			if ($response->validationResult->isValidNumber != 'true') {
+				throw new \Exception('Telefonne cislo nie je validne');
+			}
+
+			$phone = new Entity\Contacts\Phone;
+			$phone->setPhone($this->prepareNumber($response->formattingResults->E164))
+				->setInternational($response->formattingResults->international)
+				->setNational($response->formattingResults->national)
+				->setRegion($response->validationResult->phoneNumberForRegion);
+
+			//TODO: este persiste a flush
+			//$this->phoneRepository->get()->
+		}
+
+		return $phone;
+	}
+
+	/**
+	 * Vycisti a pripravi cislo na vyhladavanie
+	 * @param string $number
+	 * @return int
+	 */
+	private function prepareNumber($number) {
+		return (int)str_replace(array(' ', '+'), array(null, null), $number);
+	}
+
+	/**
+	 * Vyrvoti GET request na online sluzbu a vracia JSON response
+	 * @param string $number
+	 * @return int
+	 */
+	private function serviceRequest($number) {
+		$query = http_build_query(array(
+			'phoneNumber' => $this->prepareNumber($number)
+		));
+		$response = file_get_contents($this->serviceUrl . $query);
+		$response = Nette\Utils\Json::decode(str_replace('\'', '"', $response));
+		return $response;
+	}
+}
--- app/models/Entity/Contacts/Phone.php
+++ app/models/Entity/Contacts/Phone.php
@@ -14,10 +14,28 @@ class Phone extends \Entity\BaseEntity {
 
 	/**
 	 * @var string
-	 * @ORM\Column(type="string")
+	 * @ORM\Column(type="string", length=20)
 	 */
 	protected $phone;
 		
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=20)
+	 */
+	protected $international;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=20)
+	 */
+	protected $national;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=2)
+	 */
+	protected $region;
+
 	/**
 	 * @param string
 	 * @return Phone
@@ -32,5 +50,53 @@ class Phone extends \Entity\BaseEntity {
 	 */
 	public function getPhone() {
 		return $this->phone;
+	}
+
+	/**
+	 * @param string
+	 * @return International
+	 */
+	public function setInternational($international) {
+		$this->international = $international;
+		return $this;
+	}
+		
+	/**
+	 * @return string|NULL
+	 */
+	public function getInternational() {
+		return $this->international;
+	}
+
+	/**
+	 * @param string
+	 * @return Region
+	 */
+	public function setRegion($region) {
+		$this->region = $region;
+		return $this;
+	}
+		
+	/**
+	 * @return string|NULL
+	 */
+	public function getRegion() {
+		return $this->region;
+	}
+
+	/**
+	 * @param string
+	 * @return National
+	 */
+	public function setNational($national) {
+		$this->national = $national;
+		return $this;
+	}
+		
+	/**
+	 * @return string|NULL
+	 */
+	public function getNational() {
+		return $this->national;
 	}
 }
