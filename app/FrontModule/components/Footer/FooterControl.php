e398540d6691f1316677b6cfa9e5483189c28546
David Drurika <tralandia.david@gmail.com>
2012-12-04 09:08:11 +0100

    fix

--- app/FrontModule/components/Footer/FooterControl.php
+++ app/FrontModule/components/Footer/FooterControl.php
@@ -28,8 +28,8 @@ class FooterControl extends \BaseModule\Components\BaseControl {
 		$testimonials = $this->userSiteOwnerReviewRepositoryAccessor->get()->findBy(array('location' => $this->location), array('id' => 'DESC'), 2);
 		$template->testimonials = $testimonials;
 
-		$lastRegisteredObjects = $this->rentalRepositoryAccessor->get()->findBy(array('location' => $this->location), array('id' => 'DESC'), 5);
-		$template->lastRegisteredObjects = $lastRegisteredObjects
+		$lastRegisteredObjects = $this->rentalRepositoryAccessor->get()->findBy(array('primaryLocation' => $this->location), array('id' => 'DESC'), 5);
+		$template->lastRegisteredObjects = $lastRegisteredObjects;
 
 		$template->render();
 	}
