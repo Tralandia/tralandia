15c17458bce595ebad3bc31a2e387228e5906642
David Drurika <tralandia.david@gmail.com>
2012-12-04 09:00:59 +0100

    footer 22967

--- app/FrontModule/components/Footer/FooterControl.php
+++ app/FrontModule/components/Footer/FooterControl.php
@@ -6,10 +6,12 @@ use Nette\Application\UI\Control;
 class FooterControl extends \BaseModule\Components\BaseControl {
 
 	public $userSiteOwnerReviewRepositoryAccessor;
+	public $rentalRepositoryAccessor;
 	public $location;
 
 	public function inject(\Nette\DI\Container $dic) {
 		$this->userSiteOwnerReviewRepositoryAccessor = $dic->userSiteOwnerReviewRepositoryAccessor;
+		$this->rentalRepositoryAccessor = $dic->rentalRepositoryAccessor;
 	}
 
 	public function __construct(\Entity\Location\Location $location) {
@@ -25,6 +27,9 @@ class FooterControl extends \BaseModule\Components\BaseControl {
 		
 		$testimonials = $this->userSiteOwnerReviewRepositoryAccessor->get()->findBy(array('location' => $this->location), array('id' => 'DESC'), 2);
 		$template->testimonials = $testimonials;
+
+		$lastRegisteredObjects = $this->rentalRepositoryAccessor->get()->findBy(array('location' => $this->location), array('id' => 'DESC'), 5);
+		$template->lastRegisteredObjects = $lastRegisteredObjects
 
 		$template->render();
 	}
