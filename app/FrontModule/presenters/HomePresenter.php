5ac8cd817b0f175a1ae1ce1b26e8db4fa5c2aefa
David Drurika <tralandia.david@gmail.com>
2012-12-12 08:35:46 +0100

    zrusenie tabot #23282

--- app/FrontModule/presenters/HomePresenter.php
+++ app/FrontModule/presenters/HomePresenter.php
@@ -25,11 +25,6 @@ class HomePresenter extends BasePresenter {
 			$rentals[$rental->id]['entity'] = $rental;
 		}
 
-		$regions = $this->locationRepositoryAccessor->get()->findBy(array(
-				'parent' => 58
-			), null , 50);
-
-		$this->template->regions = array_chunk($regions,ceil(count($regions)/3));
 		$this->template->rentals = $rentals;
 
 	}
@@ -37,40 +32,6 @@ class HomePresenter extends BasePresenter {
 	public function createComponentCountryMap($name) {
 
 		return new \FrontModule\Components\CountryMap\CountryMap($this->locationRepository, $this->locationTypeRepository);
-
-	}
-
-	public function createComponentTabControl($name) {
-
-		$tabBar = new \BaseModule\Components\TabControl\TabControl();
-
-/*		$content = new \FrontModule\Components\Rentals\TopRentals($this->rentalRepository);
-		$tab = $tabBar->addTab('top');
-		$tab->setHeading(806);
-		$tab->setContent($content);
-
-		$content = new \FrontModule\Components\RegionsPage\Regions($this->locationRepository, $this->locationTypeRepository);
-		$tab = $tabBar->addTab('regions');
-		$tab->setHeading(678);
-		$tab->setContent($content)->setActive();
-
-		$content = new \FrontModule\Components\LocalitiesPage\Localities($this->locationRepository, $this->locationTypeRepository);
-		$tab = $tabBar->addTab('localities');
-		$tab->setHeading(725);
-		$tab->setContent($content);
-
-		$content = new \FrontModule\Components\TagsPage\Tags();
-		$tab = $tabBar->addTab('tags');
-		$tab->setHeading(727);
-		$tab->setContent($content);
-
-		$content = new \FrontModule\Components\GenericPage\Generic();
-		$content->slug = 'about';
-		$tab = $tabBar->addTab('about');
-		$tab->setHeading(1163);
-		$tab->setContent($content);
-*/
-		return $tabBar;
 
 	}
 
