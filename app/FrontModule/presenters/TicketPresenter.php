b91f3fb9cabec8affac38d474a39a13ab8e62c6b
Gregor Demo <demo.gregor@gmail.com>
2013-01-04 09:09:12 +0100

    ticket start

---
+++ app/FrontModule/presenters/TicketPresenter.php
@@ -0,0 +1,14 @@
+<?php 
+namespace FrontModule;
+
+use Model\Rental\IRentalDecoratorFactory;
+use FrontModule\Forms\Rental\IReservationFormFactory;
+
+class TicketPresenter extends BasePresenter {
+
+	public function renderDefault() {
+
+		
+
+	}	
+}
