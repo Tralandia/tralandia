ef6ddd78e85cc9a24cb97259dbf67e7cc7ebb80b
Gregor Demo <demo.gregor@gmail.com>
2013-01-07 13:39:17 +0100

    #23292 contact page FAQ

--- app/FrontModule/presenters/ContactPresenter.php
+++ app/FrontModule/presenters/ContactPresenter.php
@@ -3,10 +3,16 @@ namespace FrontModule;
 
 class ContactPresenter extends BasePresenter {
 
+	protected $faqCategoryRepositoryAccessor;
+
+	public function injectDic(\Nette\DI\Container $dic) {
+		$this->faqCategoryRepositoryAccessor = $dic->faqCategoryRepositoryAccessor;
+	}
+
 
 	public function renderDefault() {
 
-		
+		$this->template->categories = $this->faqCategoryRepositoryAccessor->get()->findAll();
 
 	}
 }
