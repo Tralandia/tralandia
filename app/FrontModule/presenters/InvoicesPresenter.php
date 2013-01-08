2907e4d67ba90ae02dc55746fa765897d62da608
Gregor Demo <demo.gregor@gmail.com>
2013-01-08 10:56:48 +0100

    #23836 invoices list FE template

---
+++ app/FrontModule/presenters/InvoicesPresenter.php
@@ -0,0 +1,14 @@
+<?php 
+namespace FrontModule;
+
+use Model\Rental\IRentalDecoratorFactory;
+use FrontModule\Forms\Rental\IReservationFormFactory;
+
+class InvoicesPresenter extends BasePresenter {
+
+	public function renderDefault() {
+
+	}
+
+	
+}
