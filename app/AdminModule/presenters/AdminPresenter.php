d2ca41985f53aedf48b1c75a5bbf7cafea2ab7cb
Branislav Vaculčiak <vaculciak@softone.sk>
2013-01-07 20:31:20 -0800

    edit

--- app/AdminModule/presenters/AdminPresenter.php
+++ app/AdminModule/presenters/AdminPresenter.php
@@ -8,41 +8,53 @@ class AdminPresenter extends BasePresenter {
 
 	protected $settings;
 
+	public function getEntityName() {
+		return '\\Entity\\' . ucfirst($this->getConfigName());
+	}
+
 	public function getServiceName() {
-		$parts = explode(':', $this->name);
+		return '\\Service\\' . ucfirst($this->getConfigName()) . 'Service';
+	}
+
+	public function getConfigName() {
+		$parts = explode(':', $this->name);;
 		return strtolower(end($parts));
 	}
 
 	public function startup() {
 		parent::startup();
 		
-		$this->settings = $this->getService('presenter.' . $this->getServiceName() . '.settings');
+		$this->settings = $this->getService('presenter.' . $this->getConfigName() . '.settings');
 		$this->template->settings = $this->settings;
-		
+
 	}
 
 	public function actionList() {
-		$this->settings->test = "ide toooo!!!";
 
-		/*
-		$repo = $this->context->model->getRepository('Entity\Currency');
-		$entity = $repo->find(2);
-		$this->template->form = $this->getForm('currency', $entity);
-		*/
-
-/*
-		$phoneBook = $this->getService('phoneBook');
-		$phone = '0949 888 999';
-
-		debug($phoneBook->find($phone));
-		debug($phoneBook->getOrCreate($phone));
-		debug($phoneBook->find($phone));
-*/
-
-		// $repo = $this->context->model->getRepository('Entity\Test');
-		// $entity = $repo->createNew();
-		// $this->context->model->persist($entity);
-		// $this->template->form = $this->getForm('test', $entity);
 	}
 
+	public function actionAdd() {
+		$repo = $this->context->model->getRepository($this->getEntityName());
+		$entity = $repo->createNew();
+		$this->context->model->persist($entity);
+		$this->template->form = $this->getForm($this->getConfigName(), $entity);
+		$this->settings->name = 'novééé';
+	}
+
+	public function actionEdit($id) {
+		$repo = $this->context->model->getRepository($this->getEntityName());
+		$entity = $repo->find($id);
+		$this->template->form = $this->getForm($this->getConfigName(), $entity);
+		$this->settings->name = $entity->id . ' - dynamika';
+	}
+
+	public function getForm($name, $entity) {
+		$model = $this->getService('model');
+		$form = $this->getService("presenter.$name.form")->create($entity);
+		$form->onSuccess[] = function($form) use ($model) {
+			$model->flush();
+		};
+		$this->addComponent($form, $name);
+		return $form;
+	}
 }
