498fcff15b58981dd237a0bf33772332b676bc13
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-01-17 15:59:38 +0100

    TicketForm zaklady

--- app/FrontModule/presenters/TicketPresenter.php
+++ app/FrontModule/presenters/TicketPresenter.php
@@ -4,15 +4,44 @@ namespace FrontModule;
 
 class TicketPresenter extends BasePresenter {
 
+	/**
+	 * @autowire
+	 * @var \FrontModule\Forms\ITicketFormFactory
+	 */
+	protected $ticketFormFactory;
+
 	public $ticketRepositoryAccessor;
-	public $ticketMessageRepositoryAccessor;
+
+	/**
+	 * @var \Entity\Ticket\Ticket
+	 */
+	public $ticket;
 
 	public function injectDic(\Nette\DI\Container $dic) {
 		$this->ticketRepositoryAccessor = $dic->ticketRepositoryAccessor;
-		$this->ticketMessageRepositoryAccessor = $dic->ticketMessageRepositoryAccessor;
 	}
 
-	public function renderDefault() {
+	public function actionShow($id) {
+		$ticket = $this->ticketRepositoryAccessor->get()->find($id);
 
-	}	
+		if(!$ticket) {
+			$this->redirect('Home:');
+		}
+		$this->ticket = $ticket;
+
+		$this->template->ticket = $ticket;
+	}
+
+	protected function createComponentTicketForm()
+	{
+		$form = $this->ticketFormFactory->create($this->loggedUser, $this->ticket);
+
+		$form->onSuccess[] = function ($form) {
+			if ($form->valid) $form->presenter->redirect('this');
+		};
+
+		return $form;
+	}
+
+
 }
