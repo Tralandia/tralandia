00e85e0f1f7f61bb5a524f2d55d624ae78080bb4
Gregor Demo <demo.gregor@gmail.com>
2013-01-22 15:13:21 +0100

    form elements

--- app/FrontModule/presenters/InvoicesPresenter.php
+++ app/FrontModule/presenters/InvoicesPresenter.php
@@ -7,6 +7,19 @@ class InvoicesPresenter extends BasePresenter {
 	public function renderDefault() {
 
 		
-
+		$this->templates->price = array(
+				1 => array(
+						'name' => 'sk',
+						'value' => 'slovenska koruna'
+					),
+				2 => array(
+						'name' => 'pl',
+						'value' => 'groš'
+					),
+				3 => array(
+						'name' => 'it',
+						'value' => 'talianska domacnost'
+					),							
+			);
 	}	
 }
