e001968788c2af300c06f2dafc50814e5302e08c
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-02-12 13:19:22 +0100

    PhoneContainer

--- app/AdminModule/presenters/ExampleFormPresenter.php
+++ app/AdminModule/presenters/ExampleFormPresenter.php
@@ -60,6 +60,7 @@ class ExampleFormPresenter extends BasePresenter {
 
 
 		$form['price'] = new \Extras\Forms\Container\PriceContainer('Price', ['EUR', 'HUF', 'CZK']);
+		$form['phone'] = new \Extras\Forms\Container\PhoneContainer('Phone', ['+ 421', '+ 1', '+ 2']);
 
 		$locations = $this->locationRepository->getCountriesForSelect();
 		$address = $this->addressRepository->find(1);
