e4b86b462787dbbac2c67280facf67445dc3860c
Gregor Demo <demo.gregor@gmail.com>
2013-02-19 10:02:19 +0100

    save

--- public/packages/Front/js/jquery-tra-map.js
+++ public/packages/Front/js/jquery-tra-map.js
@@ -36,9 +36,66 @@
 		            map: map
 		        });
 
-  			
+		        console.log(lat+' '+lng);
+
+		        var delta = 0.4;
+
+		        var MinLat = lat - delta;
+		        var MaxLat = lat + delta;
+
+		        var MinLng = lng - delta;
+		        var MaxLng = lng + delta;
+
+
+		        var findPanoramio = {
+		        	set: 'public',
+		        	from: 0,
+		        	to: 12,
+		        	minx: MinLng,
+		        	miny: MinLat,
+		        	maxy: MaxLat,
+		        	maxx: MaxLng,
+		        	size: 'medium',
+		        	mapfilter: true
+		        };
+
+
+		        /*
+		        $.ajax({
+					  dataType: "jsonp",
+					  crossDomain: true,
+					  data: findPanoramio,
+					  url: 'http://www.panoramio.com/map/get_panoramas.php',					  
+					  success: function(data){
+
+					  }
+					}).done(function(d){
+						
+
+						var html = '';
+						$.each(d.photos,function(k,v){
+							console.log(v);
+
+							
+							var myLatlng = new google.maps.LatLng(v.latitude,v.longitude);
+
+					        var marker = new google.maps.Marker({
+					            position: myLatlng,
+					            map: map
+					        });
+							
+
+							html+= '<li style="background-image:url('+v.photo_file_url+');"></li>';
+
+						});
+
+						$('#placesImg').html(html);
+					});
+
+			*/
 
   		}
 
   };
 })(jQuery);
+
