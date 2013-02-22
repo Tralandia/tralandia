d3c2da19b0e143ca735a4e560571792f30013a77
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-02-22 08:50:53 +0100

    #64

---
+++ app/models/Repository/User/RentalReservationRepository.php
@@ -0,0 +1,25 @@
+<?php
+namespace Repository\User;
+
+use Entity\Rental\Rental;
+
+/**
+ * RentalReservationRepository class
+ *
+ * @author Dávid Ďurika
+ */
+class RentalReservationRepository extends \Repository\BaseRepository {
+
+	public function findByRental(Rental $rental)
+	{
+		$qb = $this->_em->createQueryBuilder();
+		$qb->select('e')
+			->from($this->_entityName, 'e')
+			->andWhere($qb->expr()->eq('e.rental', ':rental'))->setParameter('rental', $rental);
+
+		return $qb->getQuery()->getResult();
+
+	}
+
+
+}
