f8313d3931bca830a7838d22a84b3b51422b3208
Gregor Demo <demo.gregor@gmail.com>
2013-02-28 17:17:21 +0100

    calendar widget pripravene pre redesign

--- public/packages/front/calendarWidget/js/calendarWidget.js
+++ public/packages/front/calendarWidget/js/calendarWidget.js
@@ -28,10 +28,57 @@
         return this.each(function(){
             (new $.calendarWidgetControl(this, radius, options));
 
-            $(this).css('background','red');
+            var self = this;
+            var $self = $(this);
+            
+            var $viewer = $('#calendarIframeViewer');
 
-            $(this).find('select').on('change',function(){
-                console.log('init');
+            
+
+            var rightIframeMargin = 10;
+            var marginTopBody = 10;
+
+            var $outputUser = $('#htmlOutputFromViewer');
+
+
+
+            $self.find('select').on('change',function(){
+
+                var calendarWidth = 136;
+                var calendarHeight = 150;
+
+                var data = {
+                    rows: $self.find('[name=rows]').val(),
+                    cells: $self.find('[name=cells]').val(),
+                    count: 0,
+                    iso: $self.find('[name=iso]').val()
+                };
+
+                
+
+                //console.log(data);
+
+                if(data.cells > 0 & data.rows > 0 & data.iso != 0){
+                    data.count = data.cells * data.rows;
+                    
+                    calendarWidth = (calendarWidth*data.cells)+rightIframeMargin;
+                    calendarHeight = (calendarHeight*(data.count/data.cells))+marginTopBody;
+
+                    $viewer.css({
+                        width: calendarWidth,
+                        height:calendarHeight
+                    }).attr({
+                        src: rawUrl
+                    });
+
+                    var rawUrl = 'http://www.'+data.iso+'.tra.com/front/calendar-iframe?rentalId=1&monthsCount='+data.count;
+
+                    var iframeForIser = '<iframe scrolling="no" style="width:'+calendarWidth+'px ;height:'+calendarHeight+'px; border:none; margin:0 auto;" src="'+rawUrl+'"></iframe>';
+                        $outputUser.val(iframeForIser);                    
+                }
+
+
+                    
             });
 
         });
@@ -42,4 +89,5 @@
 
 $(document).ready(function(){
      $('#calendarWidgetControl').calendarWidgetControl();
+
 });
