71c6e05a4ef05d3f52deb5c71b9a4d0dca127e0f
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-03-05 10:01:52 +0100

    #175

--- app/OwnerModule/presenters/CalendarWidgetPresenter.php
+++ app/OwnerModule/presenters/CalendarWidgetPresenter.php
@@ -3,6 +3,9 @@
 namespace OwnerModule;
 
 
+use Nette\Application\BadRequestException;
+use Nette\Utils\Html;
+
 class CalendarWidgetPresenter extends BasePresenter {
 
 	public function actionDefault()
@@ -10,9 +13,30 @@ class CalendarWidgetPresenter extends BasePresenter {
 
 		$this->template->rentals = $this->loggedUser->getRentals();
 		$this->template->linkTemplate = $this->link(
-			'//:Front:CalendarIframe:default',
-			['rentalId' => '__rentalId__', 'monthsCount' => '__monthsCount__']
+			':generateCode',
+			['rentalId' => '__rentalId__', 'wLanguage' => '__language__', 'columns' => '__columns__', 'rows' => '__rows__']
 		);
+	}
+
+	public function actionGenerateCode($rentalId, $wLanguage, $columns, $rows)
+	{
+		$language = $this->languageRepositoryAccessor->get()->find($wLanguage);
+		if(!$language) {
+			throw new BadRequestException;
+		}
+
+		$monthsCount = $columns * $rows;
+
+		$link = $this->link(
+			'//:Front:CalendarIframe:default',
+			['language' => $language, 'rentalId' => $rentalId, 'monthsCount' => $monthsCount]
+		);
+
+		$code = Html::el('iframe')
+			->src($link);
+
+		$json = ['code' => "$code"];
+		$this->sendJson($json);
 	}
 
 }
