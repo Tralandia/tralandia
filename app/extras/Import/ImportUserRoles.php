84218704b8914c43cc6b123550e44fe229b80630
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-04-02 11:00:24 +0200

    closes #419

--- app/extras/Import/ImportUserRoles.php
+++ app/extras/Import/ImportUserRoles.php
@@ -15,20 +15,52 @@ class ImportUserRoles extends BaseImport {
 
 	public function doImport($subsection = NULL) {
 
-		$allRoles = array(Role::GUEST, Role::OWNER, Role::TRANSLATOR, Role::SUPERADMIN);
+		$allRoles = array(
+			Role::GUEST => NULL,
+			Role::LOGGED => [
+				Role::OWNER => NULL,
+				Role::TRANSLATOR => NULL,
+				Role::SUPERADMIN => NULL,
+			],
+		);
 
-		foreach ($allRoles as $key => $value) {
-			$role = $this->context->userRoleEntityFactory->create();
-			$role->name = ucfirst($value);
-			$role->slug = $value;
-			if (in_array($value, array(Role::TRANSLATOR, Role::SUPERADMIN))) {
-				$role->homePage = ':Admin:Rental:list';
-			} else if($value == Role::OWNER) {
-				$role->homePage = ':Owner:Rental:firstRental';
-			}
-			$this->model->persist($role);
-		}
+		$this->importRoles($allRoles);
+
 		$this->model->flush();
 		$this->savedVariables['importedSections']['userRoles'] = 1;
+	}
+
+	public function importRoles($roles, Role $parent = NULL)
+	{
+		foreach ($roles as $roleName => $children) {
+			$role = $this->addRole($roleName, $parent);
+
+			$this->model->persist($role);
+
+			if(is_array($children)) {
+				$this->importRoles($children, $role);
+			}
+		}
+
+	}
+
+	public function addRole($name, Role $parent = NULL)
+	{
+		$role = $this->context->userRoleEntityFactory->create();
+
+		$role->name = ucfirst($name);
+		$role->slug = $name;
+
+		if($parent) {
+			$role->parent = $parent;
+		}
+
+		if (in_array($name, array(Role::TRANSLATOR, Role::SUPERADMIN))) {
+			$role->homePage = ':Admin:Rental:list';
+		} else if($name == Role::OWNER) {
+			$role->homePage = ':Owner:Rental:firstRental';
+		}
+
+		return $role;
 	}
 }
