e8e189891d3276766aba309912763a16a5e36eda
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-05-07 08:04:27 +0200

    save

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -37,7 +37,7 @@ Kdyby\Replicator\Container::register();
 Kdyby\Redis\DI\RedisExtension::register($configurator);
 
 $configurator->addConfig(APP_DIR . '/configs/config.neon', FALSE);
-if(\Nette\Utils\Strings::endsWith($_SERVER['HTTP_HOST'], '.tralandia.org')){
+if(\Nette\Utils\Strings::endsWith($_SERVER['HTTP_HOST'], '.tralandia.org')) {
 	$configurator->addConfig(APP_DIR . '/configs/production.config.neon', FALSE);
 } else {
 	$configurator->addConfig(APP_DIR . '/configs/local.config.neon', FALSE);
