c6a795cbd8bfcb6ade6cfdaef083347f837a5cf8
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-05-14 17:42:17 +0200

    #767

--- app/AdminModule/presenters/PhrasePresenter.php
+++ app/AdminModule/presenters/PhrasePresenter.php
@@ -3,6 +3,8 @@
 namespace AdminModule;
 
 
+use Entity\User\Role;
+
 class PhrasePresenter extends BasePresenter {
 
 	/**
@@ -10,6 +12,18 @@ class PhrasePresenter extends BasePresenter {
 	 * @var \BaseModule\Forms\ISimpleFormFactory
 	 */
 	protected $simpleFormFactory;
+
+	/**
+	 * @autowire
+	 * @var \ResultSorter
+	 */
+	protected $resultSorter;
+
+	/**
+	 * @autowire
+	 * @var \SupportedLanguages
+	 */
+	protected $supportedLanguages;
 
 	protected $phraseRepositoryAccessor;
 
@@ -26,21 +40,47 @@ class PhrasePresenter extends BasePresenter {
 
 	public function actionEditList()
 	{
+		$this->checkPermission($this->name, $this->action);
 
 	}
 
 
 	protected function createComponentPhraseEditForm()
 	{
-		$phrases = $this->phraseRepositoryAccessor->get()->findBy(['ready' => true], [], 5);
+		$phrases = $this->phraseRepositoryAccessor->get()->findBy(['type' => 9], [], 5);
 
+		$phraseContainerSettings = [];
+		if($this->user->isInRole(Role::TRANSLATOR)) {
+			$translatorLanguages = $this->languageRepositoryAccessor->get()->findByTranslator($this->loggedUser);
+			$translatorLanguages = $this->resultSorter->translateAndSort($translatorLanguages);
+			$phraseContainerSettings['translatorLanguages'] = $translatorLanguages;
+
+			$toLanguages = \Tools::arrayMap(
+				$translatorLanguages,
+				function($value) {return $value->getIso();},
+				function($key, $value) {return $value->getId();}
+			);
+			$phraseContainerSettings['disableSourceLanguageInput'] = TRUE;
+			$phraseContainerSettings['disableReadyInput'] = TRUE;
+			$phraseContainerSettings['disableCorrectedInput'] = TRUE;
+			$showOptions = FALSE;
+		} else {
+			$toLanguages = $this->supportedLanguages->getForSelect();
+			$showOptions = TRUE;
+		}
 
 		$form = $this->simpleFormFactory->create();
+
+
+
+		$form->addSelect('toLanguages', '', $toLanguages);
+		$form->addCheckbox('showOptions', '')->setDefaultValue($showOptions);
+
 
 		$listContainer = $form->addContainer('list');
 		foreach($phrases as $phrase) {
 			$phraseContainer = $listContainer->addPhraseContainer($phrase->getId(), $phrase);
-			$phraseContainer->build();
+			$phraseContainer->build($phraseContainerSettings);
 			$phraseContainer->setDefaultValues();
 		}
 
--- app/BaseModule/presenters/SecuredPresenter.php
+++ app/BaseModule/presenters/SecuredPresenter.php
@@ -12,8 +12,8 @@ abstract class SecuredPresenter extends BasePresenter {
 		 	$this->redirect(':Front:Sign:in');
 		 } else {
 		 	list($module, ) = explode(':', $this->name, 2);
-		 	if (!$this->user->isAllowed($module.'Module', $this->action)) {
-		 		$this->accessDeny();
+		 	if (!$this->user->isAllowed($module.'Module') && !$this->user->isAllowed($this->name, $this->action)) {
+		 		$this->accessDeny($this->name, $this->action);
 		 		//$this->restoreRequest($this->getPreviousBackLink());
 		 	}
 		 }
