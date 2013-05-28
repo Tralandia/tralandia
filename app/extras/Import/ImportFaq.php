90ba2ab0845efc5bd488b76d58e859b3a2652e08
Radoslav Toth <radoslavtoth@Radoslavs-MacBook-Pro.local>
2013-05-28 16:29:40 +0200

    a

--- app/extras/Import/ImportFaq.php
+++ app/extras/Import/ImportFaq.php
@@ -20,42 +20,20 @@ class ImportFaq extends BaseImport {
 
 		// Category 1
 		$faqCategory = $context->faqCategoryRepositoryAccessor->get()->createNew(FALSE);
-		$faqCategory->name = $this->createPhraseFromString('\Faq\Category', 'name', 'NATIVE', 'Category One Lorem Ipsum', $en);
+		$faqCategory->name = $this->createPhraseFromString('\Faq\Category', 'name', 'NATIVE', 'General questions', $en);
 		$this->model->persist($faqCategory);
 
-		// Question 1/1
-		$faqQuestion = $context->faqQuestionRepositoryAccessor->get()->createNew(FALSE);
-		$faqQuestion->category = $faqCategory;
-		$faqQuestion->question = $this->createPhraseFromString('\Faq\Question', 'question', 'NATIVE', 'This is my first question to first category', $en);
-		$faqQuestion->answer = $this->createPhraseFromString('\Faq\Question', 'answer', 'NATIVE', 'This is the answer to the first question in first category', $en);
-		$this->model->persist($faqQuestion);
+		$questionType = $this->createPhraseType('\Faq\Question', 'question', 'NATIVE');
+		$answerType = $this->createPhraseType('\Faq\Question', 'answer', 'NATIVE');
 
-		// Question 1/2
-		$faqQuestion = $context->faqQuestionRepositoryAccessor->get()->createNew(FALSE);
-		$faqQuestion->category = $faqCategory;
-		$faqQuestion->question = $this->createPhraseFromString('\Faq\Question', 'question', 'NATIVE', 'This is my second question to first category', $en);
-		$faqQuestion->answer = $this->createPhraseFromString('\Faq\Question', 'answer', 'NATIVE', 'This is the answer to the second question in first category', $en);
-		$this->model->persist($faqQuestion);
-
-
-		// Category 2
-		$faqCategory = $context->faqCategoryRepositoryAccessor->get()->createNew(FALSE);
-		$faqCategory->name = $this->createPhraseFromString('\Faq\Category', 'name', 'NATIVE', 'Category 2222 Lorem Ipsum', $en);
-		$this->model->persist($faqCategory);
-
-		// Question 2/1
-		$faqQuestion = $context->faqQuestionRepositoryAccessor->get()->createNew(FALSE);
-		$faqQuestion->category = $faqCategory;
-		$faqQuestion->question = $this->createPhraseFromString('\Faq\Question', 'question', 'NATIVE', 'This is my first question to second category', $en);
-		$faqQuestion->answer = $this->createPhraseFromString('\Faq\Question', 'answer', 'NATIVE', 'This is the answer to the first question in second category', $en);
-		$this->model->persist($faqQuestion);
-
-		// Question 2/2
-		$faqQuestion = $context->faqQuestionRepositoryAccessor->get()->createNew(FALSE);
-		$faqQuestion->category = $faqCategory;
-		$faqQuestion->question = $this->createPhraseFromString('\Faq\Question', 'question', 'NATIVE', 'This is my second question to second category', $en);
-		$faqQuestion->answer = $this->createPhraseFromString('\Faq\Question', 'answer', 'NATIVE', 'This is the answer to the second question in second category', $en);
-		$this->model->persist($faqQuestion);
+		$questions = q('select * from faq');
+		while ($question = mysql_fetch_array($questions)) {
+			$faqQuestion = $context->faqQuestionRepositoryAccessor->get()->createNew(FALSE);
+			$faqQuestion->category = $faqCategory;
+			$faqQuestion->question = $this->createNewPhrase($questionType, $x['question_dic_id'], NULL, NULL, $en);
+			$faqQuestion->answer = $this->createNewPhrase($answerType, $x['answer_dic_id'], NULL, NULL, $en);
+			$this->model->persist($faqQuestion);
+		}
 
 		$this->model->flush();
 	}
