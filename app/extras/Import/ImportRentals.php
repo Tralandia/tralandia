004ddb872b028e4736ecb08963f8f76d1fa5ff7b
Radoslav Toth <radoslavtoth@Radoslavs-MacBook-Pro.local>
2013-05-30 12:32:55 +0200

    IMport - images paring ON

--- app/extras/Import/ImportRentals.php
+++ app/extras/Import/ImportRentals.php
@@ -339,18 +339,18 @@ class ImportRentals extends BaseImport {
 			$model->persist($rental);
 
 
-			// // Images
-			// $temp = array_unique(array_filter(explode(',', $x['photos'])));
-			// if (is_array($temp) && count($temp)) {
-			// 	foreach ($temp as $key => $value) {
-			// 		$t = qNew('select from __importImages where oldRentalId = '.$x['id'].' and status = "imported" and oldPath = "'.$value.'"');
-			// 		if (mysql_num_rows($t) == 0) continue;
-			// 		$img = mysql_fetch_array($t);
+			// Images
+			$temp = array_unique(array_filter(explode(',', $x['photos'])));
+			if (is_array($temp) && count($temp)) {
+				foreach ($temp as $key => $value) {
+					$t = qNew('select from __importImages where oldRentalId = '.$x['id'].' and status = "imported" and oldPath = "'.$value.'"');
+					if (mysql_num_rows($t) == 0) continue;
+					$img = mysql_fetch_array($t);
 
-			// 		$rentalImage = $context->rentalImageRepositoryAccessor->get()->findOneByFilePath($img['newPath']);
-			// 		$rental->addImage($rentalImage);
-			// 	}
-			// }
+					$rentalImage = $context->rentalImageRepositoryAccessor->get()->findOneByFilePath($img['newPath']);
+					$rental->addImage($rentalImage);
+				}
+			}
 
 			// Classification
 			if ($x['classification'] > 0 && $x['classification'] != 9 ) {
