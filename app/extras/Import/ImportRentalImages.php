844ce24954bfd5c992a14f046ef33677bb06c312
Radoslav Toth <radoslavtoth@Radoslavs-MacBook-Pro.local>
2013-06-04 10:37:35 +0200

    a

--- app/extras/Import/ImportRentalImages.php
+++ app/extras/Import/ImportRentalImages.php
@@ -65,7 +65,7 @@ class ImportRentalImages extends BaseImport {
 		$context = $this->context;
 		$model = $this->model;
 		
-		$endBy = microtime(true) + 10;
+		$endBy = microtime(true) + 50;
 
 		$i = 0;
 		while(microtime(true) < $endBy) {
