b1219b8137ec509e2645c65da00d4a91785c13a7
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-06-07 14:50:41 +0200

    save

---
+++ app/services/EmailerMailer.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * This file is part of the Tralandia.
+ * User: david
+ * Created at: 6/7/13 2:16 PM
+ */
+
+use Nette;
+use Nette\Mail\Message;
+
+class EmailerMailer implements Nette\Mail\IMailer {
+
+	/**
+	 * Sends email.
+	 *
+	 * @param Nette\Mail\Message $mail
+	 *
+	 * @return void
+	 */
+	public function send(Message $mail)
+	{
+		$params = [];
+
+		$params['from_email'] = $mail->getFrom();
+		$params['from_name'] = $mail->getFrom();
+		$params['to_email'] = $mail->getHeader('Cc');
+		$params['to_name'] = $mail->getHeader('Cc');
+		$params['bcc_email'] = $mail->getHeader('Bcc');
+		$params['subject'] = $mail->getSubject();
+		$params['body'] = $mail->getBody();
+		$params['body_html'] = $mail->getHtmlBody();
+
+		$this->addEmailToEmailer($params);
+	}
+
+
+	private function addEmailToEmailer($params){
+		$insert = array();
+		foreach($params as $key => $val){
+			if(Nette\Utils\Validators::isEmail($val['to_email']) && Nette\Utils\Validators::isEmail($val['from_email'])){
+				$t = array();
+				$t[] = 1;//(int)$val['urgency'];
+				$t[] = 1;//(int)$val['confirmed'];
+				$t[] = 1;//(int)$val['batch_id'];
+				$t[] = NULL;//(int)$val['stamp'];
+				$t[] = '"'.$val['from_email'].'"';
+				$t[] = '"'.$val['from_name'].'"';
+				$t[] = '"'.$val['to_email'].'"';
+				$t[] = '"'.$val['to_name'].'"';
+				$t[] = '"'.$val['bcc_email'].'"';
+				$t[] = '"'.$val['subject'].'"';
+				$t[] = '"'.addslashes(gzcompress($val['body'])).'"';
+				$t[] = '"'.addslashes(gzcompress($val['body_html'])).'"';
+				$t[] = '"'.$val['attachments'].'"';
+				$t[] = '"'.$this->getDomainFromEmail($val['to_email']).'"';
+				$t[] = 0; // test
+
+				$insert[] = '('.implode(', ', $t).')';
+			}
+		}
+
+		if(count($insert)){
+			$query = 'INSERT INTO emailer_queued (urgency, confirmed, batch_id, stamp, from_email, from_name, to_email, to_name, bcc_email, subject, body, body_html, attachments, domain, test) VALUES '.implode(', ', $insert);
+			//ape($query);
+			return (bool)$this->qEmailer($query);
+		} else {
+			return TRUE;
+		}
+	}
+
+	private function qEmailer($query){
+		static $emailerConnection;
+		if(!$emailerConnection){
+			$config = array(
+				'host' => '93.184.77.84',
+				'user' => 'as000500',
+				'password' => 'nundilli',
+				'database' => 'as000500db',
+			);
+			$emailerConnection = mysql_connect($config['host'], $config['user'], $config['password']);
+			mysql_select_db($config['database'], $emailerConnection);
+		}
+		//if(Invoicing::$testMode) pr($query);
+
+		if($r =@mysql_query($query, $emailerConnection)){
+			if (stripos($query, "insert into emailer")!==false) {
+				$r = mysql_insert_id($emailerConnection);
+			}
+			return $r;
+		} else {
+			return FALSE;
+		}
+	}
+
+	private function getDomainFromEmail($email){
+		if(Nette\Utils\Validators::isEmail($email)){
+			$domain = strstr($email, '@');
+			$domain = substr($domain, 1, strpos($domain, '.')-1);
+		}else{
+			$domain = FALSE;
+		}
+		return $domain;
+	}
+}
