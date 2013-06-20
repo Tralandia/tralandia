b0db15014cc609ab8bbe30e077801d2e20e50ea8
Radoslav Toth <radoslavtoth@Radoslavs-MacBook-Pro.local>
2013-06-20 09:30:32 +0200

    a

--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
 //ini_set('display_errors', '1'); error_reporting(E_ALL);
 
-if (PHP_SAPI != 'cli') {
+// if (PHP_SAPI != 'cli') {
 
-	if(array_key_exists('tester', $_GET)) {
-		setcookie("tester", (int)$_GET['tester']);
-		header("Location: /");
-		die();
-	}
+// 	if(array_key_exists('tester', $_GET)) {
+// 		setcookie("tester", (int)$_GET['tester']);
+// 		header("Location: /");
+// 		die();
+// 	}
 
-	if(!array_key_exists('tester', $_COOKIE) || !$_COOKIE['tester']) {
-		require __DIR__ . '/landingPage/index.html';
-		exit;
-	}
-}
+// 	if(!array_key_exists('tester', $_COOKIE) || !$_COOKIE['tester']) {
+// 		require __DIR__ . '/landingPage/index.html';
+// 		exit;
+// 	}
+// }
 
 // the identification of this site
 define('SITE', '');
