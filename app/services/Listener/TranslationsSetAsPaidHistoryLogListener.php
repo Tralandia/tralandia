2237f388e0ac372c4eb0ba87b6cdff4a8cff18a2
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-07-02 10:35:25 +0200

    save

---
+++ app/services/Listener/TranslationsSetAsPaidHistoryLogListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Listener;
+
+
+use Entity\Language;
+use Entity\Log\History;
+use Entity\User\User;
+
+class TranslationsSetAsPaidHistoryLogListener extends BaseHistoryLogListener {
+
+	public function getSubscribedEvents()
+	{
+		return ['AdminModule\Grids\Dictionary\ManagerGrid::onMarkAsPaid'];
+	}
+
+	public function onMarkAsPaid(Language $language, $changedIds, User $requestedBy)
+	{
+		$log = $this->createLog(History::TRANSLATIONS_SET_AS_PAID);
+
+		$translations = $this->em->getRepository(TRANSLATION_ENTITY)->findByIds($changedIds);
+
+		$wordsCountToPay = $this->em->getRepository(TRANSLATION_ENTITY)->calculateWordsInTranslations($translations);
+		$translator = $language->getTranslator();
+		$data = [
+			'language' => [
+				'id' => $language->getId(),
+				'iso' => $language->getIso(),
+				'translationPrice' => $language->getTranslationPrice(),
+			],
+			'translator' => [
+				'id' => $translator ? $translator->getId() : NULL,
+				'email' => $translator ? $translator->getEmail() : NULL,
+			],
+			'requestedBy' => [
+				'id' => $requestedBy->getId(),
+				'email' => $requestedBy->getEmail(),
+			],
+			'changedIds' => $changedIds,
+			'wordsCountToPay' => $wordsCountToPay,
+			'totalPrice' => $language->getTranslationPrice() * $wordsCountToPay,
+		];
+
+		$log->setData($data);
+
+		$this->em->persist($log);
+		$this->em->flush($log);
+	}
+
+
+}
