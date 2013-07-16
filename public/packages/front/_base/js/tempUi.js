8ebf0417ba0af8c8d8eeb8ea8506f7862a889faf
Gregor Demo <demo.gregor@gmail.com>
2013-07-16 16:07:19 +0200

    detail rental redesign

--- public/packages/front/_base/js/tempUi.js
+++ public/packages/front/_base/js/tempUi.js
@@ -1,24 +1,45 @@
 $(function(){
-	console.log('temp mode');
+	// console.log('temp mode');
 
 	$(document).scroll(function(){
 		console.log();
 
 		if($('body').scrollTop() > 99){
-			$('#socialIcons').css({
-				position: 'fixed',
-				top:'0px',
-				'margin-left': '811px',
-				'border-radius': '0 0 5px 5px'
-			});
+			$('#headerShareMenu').addClass('fixed');
 		} else {
-			$('#socialIcons').css({
-				position: 'relative',
-				top: '-20px',
-				'margin-left':0,
-				'border-radius': '5px'
-			});
+			$('#headerShareMenu').removeClass('fixed');
 		}
 	});
 });
 
+
+
+(function($){
+    $.yourPluginName = function(el, options){
+
+        var base = this;
+
+        base.$el = $(el);
+        base.el = el;
+
+        base.$el.data("yourPluginName", base);
+        
+        base.init = function(){
+            
+            base.options = $.extend({},$.yourPluginName.defaultOptions, options);
+
+        };
+
+        base.init();
+    };
+    
+    $.yourPluginName.defaultOptions = {
+    };
+    
+    $.fn.yourPluginName = function(options){
+        return this.each(function(){
+            (new $.yourPluginName(this, options));
+        });
+    };
+    
+})(jQuery);
