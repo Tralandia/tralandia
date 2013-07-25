7ec6471382317a37726b44048461288ea71720c8
David Drurika <david@David-Durikas-MacBook-Pro.local>
2013-07-25 15:30:22 +0200

    save

---
+++ app/AdminModule/presenters/AmenitiesMigrationPresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Tralandia.
+ * User: david
+ * Created at: 7/25/13 3:29 PM
+ */
+
+namespace AdminModule;
+
+
+use Nette;
+
+class AmenitiesMigrationPresenter extends BasePresenter {
+
+}
