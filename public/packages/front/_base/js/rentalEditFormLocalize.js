e57e929a1e17c7ae8b9b211dd17260b32cb3b922
Gregor Demo <demo.gregor@gmail.com>
2013-08-01 08:43:55 +0200

    [Closes #1821]

--- public/packages/front/_base/js/rentalEditFormLocalize.js
+++ public/packages/front/_base/js/rentalEditFormLocalize.js
@@ -1,10 +1,18 @@
 
 
 $(function(){
-    $('.selectLanguage').on('change',function(){
+
+    $('.selectLanguageBasicInformation').on('change',function(){
         var iso = $(this).val();
-        $(this).parents('form').find('tr.toggleLanguage:not(.'+iso+')').addClass('hide');
-        $(this).parents('form').find('tr.toggleLanguage.'+iso).removeClass('hide');
+        $(this).parents('form').find('tr.toggleLanguage.selectLanguageBasicInformation:not(.'+iso+')').addClass('hide');
+        $(this).parents('form').find('tr.toggleLanguage.selectLanguageBasicInformation.'+iso).removeClass('hide');
     });
+
+    $('.selectLanguageInterview').on('change',function(){
+        var iso = $(this).val();
+        $(this).parents('form').find('tr.toggleLanguage.interview:not(.'+iso+')').addClass('hide');
+        $(this).parents('form').find('tr.toggleLanguage.interview.'+iso).removeClass('hide');
+    });   
+
 });
 
