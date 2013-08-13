df2586a114198721854a5f5c960136a985698806
David Drurika <durika.d@gmail.com>
2013-08-13 08:11:57 +0200

    savesav

--- app/FrontModule/presenters/BasePresenter.php
+++ app/FrontModule/presenters/BasePresenter.php
@@ -93,10 +93,10 @@ abstract class BasePresenter extends \BasePresenter {
 
 		$this->template->favoriteRentals = $this->favoriteList->getRentalList();
 
-		if(!$this->template->pageH1) {
+		if(!isset($this->template->pageH1)) {
 			$this->template->pageH1 = $this->pageSeo->getH1();
 		}
-		
+
 		$this->template->countryCountObjects =  $this->environment->getPrimaryLocation()->getRentalCount();
 
 		$this->template->worldwideCount = $this->locationRepositoryAccessor->get()->getWorldwideRentalCount();
