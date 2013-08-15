44bcb8ab3805bbb9704a3fc423951e1b4056b427
Gregor Demo <demo.gregor@gmail.com>
2013-08-15 08:37:59 +0200

    28726 mapa start

--- public/packages/front/rentalDetail/js/rentalDetail.js
+++ public/packages/front/rentalDetail/js/rentalDetail.js
@@ -1,87 +1,317 @@
-(function($) {
-	
-	$.fn.traMap = function() {
+(function($){
+	$.traxDetailMap = function(el, options){
 
-		// default map zoom level
-		var zoomVal = 4;
-
-		var rentalId = parseInt($(this).data('rentalId'));
-
-		if(typeof $(this).attr('zoom') != 'undefined')
-		{
-			zoomVal = parseInt($(this).attr('zoom'));
-		}
-
-		if(typeof $(this).attr('value') == 'undefined')
-		{
-			$(this).html('error');
-		} else {
+		var base = this;
+		
+		base.$el = $(el);
+		base.el = el;
+		base.markers = [];
+		base.$el.data("traxDetailMap", base);
+		
+		base.init = function(){
+			
+			
+			base.options = $.extend({},$.traxDetailMap.defaultOptions, options);
+			
 
 
-			if(typeof $('body').attr('data-google-map-render') == 'undefined' ){
+			base.options.mapZoom = base.$el.data('zoom');
 
-				var coordinates = $(this).attr('value').split(',');
-
-				var lat = parseFloat(coordinates[0]);
-				var lng = parseFloat(coordinates[1]);
-
-				var iconBase = '../../../../images/markers/';
-
-				var myLatlng = new google.maps.LatLng(lat,lng);
-				var mapOptions = {
-					zoom: zoomVal,
-					scrollwheel: false,
-					center: myLatlng,
-					mapTypeId: google.maps.MapTypeId.HYBRID
-				}
-				var map = new google.maps.Map(document.getElementById($(this).attr('id')), mapOptions);
-
-				var isFavorites = false;
-				var myFavorites = $.cookie('favoritesList');
-
-				
+			base._loadMap();
 
 
-				if(typeof myFavorites != 'undefined' && myFavorites != null)
-				{
-					myFavorites = myFavorites.split(',');
+			
 
-					$.each(myFavorites,function(k,v){
-						if(rentalId == v){
-							isFavorites = true;
-						}
-					});					
-				}
+		};
+		
+		base._loadMap = function(){
+			base.rentalId = parseInt(base.$el.data('rentalId'));
+			base.mapCoordinates = base.$el.attr('value');
+			base.mapCoordinates = base.mapCoordinates.split(',');
 
+			base.map = {
+				lat: parseFloat(base.mapCoordinates[0]),
+				lng: parseFloat(base.mapCoordinates[1])
+			};
 
-				if(isFavorites){
-					var iconName  = 'map-pointer-heart.png';
-				} else {
-					var iconName  = 'map-pointer-home.png';
-				}
+			console.log(base.map);
 
+			base.myLatlng = new google.maps.LatLng(base.map.lat,base.map.lng);
 
-				var marker = new google.maps.Marker({
-					position: myLatlng,
-					map: map,
-					icon: iconBase + iconName
-				});
+			var mapOptions = {
+				zoom: base.options.mapZoom,
+				scrollwheel: false,
+				center: base.myLatlng,
+				mapTypeId: google.maps.MapTypeId.ROUTE
+				// mapTypeId: google.maps.MapTypeId.HYBRID
+			}
 
+			base.googleMap = new google.maps.Map(document.getElementById(base.$el.attr('id')), mapOptions);
 
+			var marker = new google.maps.Marker({
+				position: base.myLatlng,
+				map: base.googleMap,
+				icon: base._getMarkerIcon()
+			});
 
 
 			$('body').attr('data-google-map-render',true);
 
+			base._bindMapListener();
 
-}
+			base._renderOtherRentals(base.mapCoordinates[0],base.mapCoordinates[1]);
 
+		};
 
+		base._bindMapListener = function(){
 
+		  google.maps.event.addListener(base.googleMap, 'dragend', base._afterMapListener);
+		  google.maps.event.addListener(base.googleMap, 'zoom_changed', base._afterMapListener);
 
-}
+		};
 
-};
+		base._afterMapListener = function(){
+		    var zoomLevel = base.googleMap.getZoom();
+		    var bounds = base.googleMap.getBounds();
+		    // @tmp
+		    var center = base.googleMap.getCenter();
+		   
+		    // console.log(base.googleMap.getCenter());
+
+		    // console.log(zoomLevel);
+		    // console.log(bounds.getNorthEast());
+		    // console.log(bounds.getSouthWest());
+
+		    // base._renderOtherRentals(center.mb,center.nb);
+		};
+
+		base._getMarkerIcon = function(type){
+
+			var isFavorites = false;
+			var myFavorites = $.cookie(base.options.favoritesCookieKey);
+
+			if(typeof myFavorites != 'undefined' && myFavorites != null)
+			{
+				myFavorites = myFavorites.split(',');
+
+				$.each(myFavorites,function(k,v){
+					if(base.rentalId == v){
+						isFavorites = true;
+					}
+				});					
+			}
+
+			var iconName = '';
+
+			if(isFavorites){
+				iconName  = base.options.iconSet.large.heart;
+			} else {
+				iconName  = base.options.iconSet.large.home;
+			} 
+
+			if(typeof type != 'undefined'){
+				switch(type){
+					case 'other-small':
+						iconName  = base.options.iconSet.small.home;
+						break;
+				}
+			}
+
+			return base.options.iconPath + iconName;    		
+		};
+	
+		//  @todo ajax callback
+		base._renderOtherRentals = function(lat,lng){
+
+			var html = '';
+
+			$.each(base._returnRentalsTmp(lat,lng),function(k,v){
+
+				html += '<li data-meta=\''+JSON.stringify(v)+'\'><img src="'+v.thumbnail+'"></li>';
+
+				base._makeMarker(v.lat,v.lng,v.name);
+
+			});
+
+			$(base.options.otherRentaliListSelector).html(html);
+
+			$(base.options.otherRentaliListSelector+' li').live('click',function(){
+				console.log($(this).data('meta'));
+			});
+
+		};
+
+		base._makeMarker = function(lat,lng,title){
+
+			var markerOptions = {
+					map: base.googleMap, 
+					position: new google.maps.LatLng(lat, lng),
+					title: title,
+				};
+			
+			var nm = new google.maps.Marker(markerOptions);
+			base.markers.push(nm);
+
+			console.log(lat);
+			console.log(lng);
+
+		};
+
+		base._returnRentalsTmp = function(lat,lng){
+
+			var data = [];
+
+			var delta = 10000;
+			var dimensions = {
+				helpLat : lat.toString().split('.'),
+				helpLng : lng.toString().split('.'),
+			}
+
+			var thubnails = [];
+				thubnails[0] = 'http://tralandiastatic.com/rental_images/2013_06/03/yn/1e/medium.jpeg';
+				thubnails[1] = 'http://tralandiastatic.com/rental_images/2013_06/05/f0/ds/medium.jpeg';
+				thubnails[2] = 'http://tralandiastatic.com/rental_images/2013_06/04/s4/85/medium.jpeg';
+				thubnails[3] = 'http://tralandiastatic.com/rental_images/2013_06/03/5p/18/medium.jpeg';
+				thubnails[4] = 'http://tralandiastatic.com/rental_images/2013_06/05/40/cq/medium.jpeg';
+				thubnails[5] = 'http://tralandiastatic.com/rental_images/2013_06/05/k6/19/medium.jpeg';
+				thubnails[6] = 'http://tralandiastatic.com/rental_images/2013_06/05/xa/u6/medium.jpeg';
+				thubnails[7] = 'http://tralandiastatic.com/rental_images/2013_06/03/of/6j/medium.jpeg';
+				thubnails[8] = 'http://tralandiastatic.com/rental_images/2013_06/05/gv/23/medium.jpeg';
+				thubnails[9] = 'http://tralandiastatic.com/rental_images/2013_06/05/2m/cj/medium.jpeg';
+
+			for(var i = 0 ; i < 9 ; ++i){
+
+				dimensions.helpLat[1] = dimensions.helpLat[1] - Math.floor(Math.random() * (delta) + 1);
+				dimensions.helpLng[1] = dimensions.helpLng[1] - Math.floor(Math.random() * (delta) + 1);
+
+				var forPush = {
+					name: 'tralala '+i,
+					id: i,
+					info1: 'Max '+Math.floor(Math.random() * (50 - 1 + 1) + 1)+' osob | '+Math.floor(Math.random() * (100 - 17 + 1) + 17)+' Eur osoba/noc',
+					info2: 'Studňa, zváračka, cukrová repa, zelovoc, cédéčka',
+					url: 'http://www.sk.tra.com/utulny-privat-kosar-v-tichom-prostredi-r21501',
+					thumbnail: thubnails[i],
+					lat: dimensions.helpLat[0]+'.'+dimensions.helpLat[1],
+					lng: dimensions.helpLng[0]+'.'+dimensions.helpLng[1],
+
+				};
+				
+				data.push(forPush);
+			}
+
+			// console.log(data);
+
+			return data;
+			
+		}
+		
+		base.init();
+	};
+	
+	$.traxDetailMap.defaultOptions = {
+		mapZoom: 4,
+		iconPath: '../../../../images/markers/',
+		favoritesCookieKey: 'favoritesList',
+		iconSet: {
+			large: {
+				heart: 'map-pointer-heart.png',
+				home: 'map-pointer-home.png'
+			},
+			small: {
+				home: 'other-rental-small.png',
+			}
+		},
+		otherRentaliListSelector: 'ul.relatedRentals',
+	};
+	
+	$.fn.traxDetailMap = function(options){
+		return this.each(function(){(new $.traxDetailMap(this, options));});};
+	
 })(jQuery);
+
+
+
+// (function($) {
+	
+// 	$.fn.traMap = function() {
+
+// 		// default map zoom level
+// 		var zoomVal = 4;
+
+// 		var rentalId = parseInt($(this).data('rentalId'));
+
+// 		if(typeof $(this).attr('zoom') != 'undefined')
+// 		{
+// 			zoomVal = parseInt($(this).attr('zoom'));
+// 		}
+
+// 		if(typeof $(this).attr('value') == 'undefined')
+// 		{
+// 			$(this).html('error');
+// 		} else {
+
+
+// 			if(typeof $('body').attr('data-google-map-render') == 'undefined' ){
+
+// 				var coordinates = $(this).attr('value').split(',');
+
+// 				var lat = parseFloat(coordinates[0]);
+// 				var lng = parseFloat(coordinates[1]);
+
+// 				var iconBase = '../../../../images/markers/';
+
+// 				var myLatlng = new google.maps.LatLng(lat,lng);
+// 				var mapOptions = {
+// 					zoom: zoomVal,
+// 					scrollwheel: false,
+// 					center: myLatlng,
+// 					mapTypeId: google.maps.MapTypeId.HYBRID
+// 				}
+// 				var map = new google.maps.Map(document.getElementById($(this).attr('id')), mapOptions);
+
+// 				var isFavorites = false;
+// 				var myFavorites = $.cookie('favoritesList');
+
+// 				if(tndefinypeof myFavorites != 'ued' && myFavorites != null)
+// 				{
+// 					myFavorites = myFavorites.split(',');
+
+// 					$.each(myFavorites,function(k,v){
+// 						if(rentalId == v){
+// 							isFavorites = true;
+// 						}
+// 					});					
+// 				}
+
+
+// 				if(isFavorites){
+// 					var iconName  = 'map-pointer-heart.png';
+// 				} else {
+// 					var iconName  = 'map-pointer-home.png';
+// 				}
+
+
+// 				var marker = new google.maps.Marker({
+// 					position: myLatlng,
+// 					map: map,
+// 					icon: iconBase + iconName
+// 				});
+
+
+
+
+// 			$('body').attr('data-google-map-render',true);
+
+
+// }
+
+
+
+
+// }
+
+// };
+// })(jQuery);
 
 
 
@@ -352,12 +582,12 @@ function rentalDetailDatepickerInit(){
 
 		dateFormat: "yy-mm-dd",
 		beforeShow: function(textbox, instance){
-            instance.dpDiv.css({
-                    marginLeft: '0px'
-            });	
+			instance.dpDiv.css({
+					marginLeft: '0px'
+			});	
 
-            onChangeCalendar(this);
-            	
+			onChangeCalendar(this);
+				
 		},
 		onChangeMonthYear: function(){
 			onChangeCalendar(this);
@@ -373,10 +603,10 @@ function rentalDetailDatepickerInit(){
 		beforeShow: function(textbox, instance){
 
 
-            instance.dpDiv.css({
-                    
-                    marginLeft: (-textbox.offsetWidth-10) + 'px'
-            });			
+			instance.dpDiv.css({
+					
+					marginLeft: (-textbox.offsetWidth-10) + 'px'
+			});			
 
 			var fromValues = $('.datepicker').val().split('-');
 
@@ -410,7 +640,8 @@ function rentalDetailDatepickerInit(){
 var global = {};
 
 global.mapInit = function(){
-	$('#objectDetailMap').traMap();
+	// $('#objectDetailMap').traMap();
+	$('#objectDetailMap').traxDetailMap();
 }
 
 function lazyLoadMap() { 
