b7a1b886027f8f4079c086d5f9ebeca6da8369f3
David Drurika <durika.d@gmail.com>
2013-09-17 08:00:16 +0200

    database cache: podpora tagov

--- libs/Tralandia/Caching/Database/DatabaseClient.php
+++ libs/Tralandia/Caching/Database/DatabaseClient.php
@@ -23,22 +23,24 @@ class DatabaseClient {
 	 */
 	private $table;
 
+	/**
+	 * @var string
+	 */
+	private $tagsTable;
+
 
 	public function __construct(\DibiConnection $connection, $table)
 	{
 		$this->connection = $connection;
 		$this->table = $table;
+		$this->tagsTable = $table . '_tag';
 	}
 
 
-	public function save($id, $value, array $tags = NULL)
+	public function save($id, $value, $expiration = NULL, array $tags = NULL)
 	{
 
 		$row = $this->find($id);
-
-		if(is_array($tags) && count($tags)) {
-			$tags = ',,' . implode(',,', $tags) . ',,';
-		}
 
 		if(is_array($value)) {
 			$value = Nette\Utils\Json::encode($value);
@@ -49,7 +51,7 @@ class DatabaseClient {
 		$args = [
 			'id' => $id,
 			'value' => gzcompress($value, 6),
-			'tags' => $tags,
+			'expiration' => $expiration,
 		];
 
 		if($row) {
@@ -57,7 +59,25 @@ class DatabaseClient {
 			$this->connection->query("UPDATE LOW_PRIORITY [{$this->table}] SET ", $args, 'WHERE [id]=%s', $id);
 		} else {
 			$this->connection->query("INSERT DELAYED INTO [{$this->table}]", $args);
+
 		}
+
+		if(is_array($tags)) {
+			$this->connection->query("DELETE FROM [{$this->tagsTable}] WHERE [cache_id] = %s", $id);
+
+			if(count($tags)) {
+				$tagValues = [];
+				foreach($tags as $tag) {
+					$tagValues[] = [
+						'cache_id' => $id,
+						'tag' => $tag,
+					];
+				}
+
+				$this->connection->query("INSERT DELAYED INTO [{$this->tagsTable}] %ex", $tagValues);
+			}
+		}
+
 	}
 
 
@@ -70,7 +90,12 @@ class DatabaseClient {
 	public function findValue($id)
 	{
 		$row = $this->find($id);
-		$value = $row ? gzuncompress($row->value) : NULL;
+
+		if(!$row || !$this->isCacheValid($row)) {
+			return NULL;
+		}
+
+		$value = gzuncompress($row->value);
 
 		try {
 			$temp = Nette\Utils\Json::decode($value, Nette\Utils\Json::FORCE_ARRAY);
@@ -82,10 +107,48 @@ class DatabaseClient {
 		return $value;
 	}
 
-
-	public function cleanByTag(array $tags)
+	public function isCacheValid($row)
 	{
-		$tags = ',,' . implode(',,', $tags) . ',,';
-		$this->connection->delete($this->table)->where('tags LIKE %~like~', $tags);
+		do {
+			$expiration = $row->expiration;
+			if($expiration !== NULL && $expiration <= time()) {
+				break;
+			}
+
+			return TRUE;
+		} while (FALSE);
+
+		$this->clearByIds([$row->id]);
+		return FALSE;
 	}
+
+
+	public function findCacheIdsByTags(array $tags)
+	{
+		$ids = $this->connection->select('c.id AS cId, count(c.id) AS count')
+			->from($this->table . ' AS c')
+			->innerJoin($this->tagsTable . ' AS t')->on('t.cache_id = c.id')
+			->where('[tag] IN %in', $tags)
+			->groupBy('c.id')
+			->having('count = %i', count($tags))
+			->fetchPairs('cId', 'cId');
+
+		return $ids;
+	}
+
+
+	public function cleanByTags(array $tags)
+	{
+		if(count($tags)) {
+			$ids = $this->findCacheIdsByTags($tags);
+			$this->clearByIds($ids);
+		}
+	}
+
+	public function clearByIds(array $ids)
+	{
+		$this->connection->delete($this->table)->where('[id] IN %in', $ids)->execute();
+		$this->connection->delete($this->tagsTable)->where('[cache_id] IN %in', $ids)->execute();
+	}
+
 }
