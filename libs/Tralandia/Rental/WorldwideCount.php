ae783125352039e4a884518da0beccb2d373fc48
David Drurika <durika.d@gmail.com>
2013-09-23 10:02:05 +0200

    save

---
+++ libs/Tralandia/Rental/WorldwideCount.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the Tralandia.
+ * User: david
+ * Created at: 9/23/13 9:42 AM
+ */
+
+namespace Tralandia\Rental;
+
+
+use Nette;
+
+class WorldwideCount {
+
+}
