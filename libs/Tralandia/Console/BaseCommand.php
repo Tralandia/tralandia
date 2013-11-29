8acd997d1ee812b3f8f1fb7683f3a788c5f4e071
macbook <macbook@macbooks-MacBook-Pro-2.local>
2013-11-29 14:29:00 +0100

    save

---
+++ libs/Tralandia/Console/BaseCommand.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the Harvester.
+ * User: david
+ * Created at: 9/11/13 1:27 PM
+ */
+
+namespace Tralandia\Console;
+
+
+use Nette;
+use Symfony\Component\Console\Command\Command;
+
+class BaseCommand extends Command {
+
+}
---
+++ libs/Tralandia/Console/EmailManagerCommand.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the tralandia.
+ * User: macbook
+ * Created at: 29/11/13 13:30
+ */
+
+namespace Tralandia\Console;
+
+
+use Nette;
+
+class EmailManagerCommand extends BaseCommand
+{
+
+
+
+}
