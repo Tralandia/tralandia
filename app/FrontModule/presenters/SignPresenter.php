6a8e0cf1d1e1879631ab176652ea84943cc1d746
macbook <macbook@macbooks-MacBook-Pro-5.local>
2014-03-24 10:19:24 +0100

    save

--- app/FrontModule/presenters/SignPresenter.php
+++ app/FrontModule/presenters/SignPresenter.php
@@ -85,24 +85,23 @@ class SignPresenter extends BasePresenter
 	{
 		$form = $this->simpleFormFactory->create();
 
-		$form->addPassword('password', 'o997')
+		$form->addPassword('password', '553')
 			->setOption('help', $this->translate('o100145'))
 			->setOption('prepend', '<i class="icon-lock"></i>')
 			->setRequired($this->translate('o100145'))
-			->addRule($form::MIN_LENGTH, $this->translate('o100145'), 5);
+			->addRule($form::MIN_LENGTH, $this->translate('1328'), 5);
 
-		$form->addPassword('password2', '!heslo znova')
+		$form->addPassword('password2', '556')
 			->setOption('prepend', '<i class="icon-lock"></i>')
-			->setRequired($this->translate('o100145'))
-			->addRule($form::MIN_LENGTH, $this->translate('o100145'), 5);
+			->setRequired($this->translate('o100145'));
 
 
-		$form->addSubmit('submit', '!zmen heslo');
+		$form->addSubmit('submit', '1318');
 
 		$form->onValidate[] = function(SimpleForm $form) {
 			$values = $form->getValues();
 			if($values->password != $values->password2) {
-				$form['password2']->addError('!hesla sa nezhoduju');
+				$form['password2']->addError($this->translate('558'));
 			}
 		};
 
@@ -127,6 +126,8 @@ class SignPresenter extends BasePresenter
 
 
 		$identity = $this->authenticator->getIdentity($user);
+
+		$this->flashMessage($this->translate(700), self::FLASH_SUCCESS);
 		$this->login($identity);
 		$this->redirect('afterLogin');
 	}
