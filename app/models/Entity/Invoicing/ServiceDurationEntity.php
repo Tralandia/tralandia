c1faa5e2ec12371233b5e50481886f5e89bd9ebf
achtan <durika.d@gmail.com>
2014-04-17 09:46:52 +0200

    Invoicing init

---
+++ app/models/Entity/Invoicing/ServiceDurationEntity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Entity\Invoicing;
+
+use Entity\Phrase;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="invoicing_serviceduration")
+ */
+class ServiceDuration extends \Entity\BaseEntity {
+
+
+}
