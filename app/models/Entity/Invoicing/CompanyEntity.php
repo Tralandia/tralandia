a58fcbb91dbd82667ff30d406432a633650604f1
achtan <durika.d@gmail.com>
2014-04-17 09:46:52 +0200

    Invoicing init

---
+++ app/models/Entity/Invoicing/CompanyEntity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Entity\Invoicing;
+
+use Entity\Phrase;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="invoicing_company")
+ */
+class Company extends \Entity\BaseEntity {
+
+
+}
---
+++ app/models/Entity/Invoicing/ServiceTypeEntity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Entity\Invoicing;
+
+use Entity\Phrase;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="invoicing_servicetype")
+ */
+class ServiceType extends \Entity\BaseEntity {
+
+
+}
