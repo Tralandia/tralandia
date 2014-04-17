f2b259e8669f3f742145c29bde669218567683c8
achtan <durika.d@gmail.com>
2014-04-17 11:00:01 +0200

    save

--- app/models/Entity/Invoicing/CompanyEntity.php
+++ app/models/Entity/Invoicing/CompanyEntity.php
@@ -11,5 +11,70 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Company extends \Entity\BaseEntity {
 
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $name;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $address;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $address2;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $postcode;
+
+	/**
+	 * @var \Entity\Location\Location
+	 * @ORM\ManyToOne(targetEntity="Entity\Location\Location")
+	 */
+	protected $locality;
+
+	/**
+	 * @var \Entity\Location\Location
+	 * @ORM\ManyToOne(targetEntity="Entity\Location\Location")
+	 */
+	protected $primaryLocation;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $companyId; // aka ICO
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $companyVatId;
+
+	/**
+	 * @var float
+	 * @ORM\Column(type="float", nullable=true)
+	 */
+	protected $vat;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $registrator;
+
+	/**
+	 * @var boolean
+	 * @ORM\Column(type="boolean")
+	 */
+	protected $inEu = TRUE;
 
 }
