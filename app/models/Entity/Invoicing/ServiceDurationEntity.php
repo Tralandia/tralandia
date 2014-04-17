4402fa5c637bb6cb9eb7db15602da6b231861465
achtan <durika.d@gmail.com>
2014-04-17 11:00:01 +0200

    save

--- app/models/Entity/Invoicing/ServiceDurationEntity.php
+++ app/models/Entity/Invoicing/ServiceDurationEntity.php
@@ -12,4 +12,34 @@ use Doctrine\ORM\Mapping as ORM;
 class ServiceDuration extends \Entity\BaseEntity {
 
 
+	/**
+	 * @var string
+	 * @ORM\Column(type="string")
+	 */
+	protected $slug;
+
+	/**
+	 * @var string
+	 * @ORM\OneToOne(targetEntity="Entity\Phrase\Phrase", cascade={"persist", "remove"})
+	 */
+	protected $name;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string")
+	 */
+	protected $strtotime;
+
+	/**
+	 * @var integer
+	 * @ORM\Column(type="integer")
+	 */
+	protected $sort = 0;
+
+	/**
+	 * @var boolean
+	 * @ORM\Column(type="boolean")
+	 */
+	protected $separatorAfter = FALSE;
+
 }
--- app/models/Entity/Invoicing/ServiceEntity.php
+++ app/models/Entity/Invoicing/ServiceEntity.php
@@ -12,4 +12,46 @@ use Doctrine\ORM\Mapping as ORM;
 class Service extends \Entity\BaseEntity {
 
 
+	/**
+	 * @var ServiceType
+	 * @ORM\ManyToOne(targetEntity="ServiceType")
+	 */
+	protected $type;
+
+	/**
+	 * @var ServiceDuration
+	 * @ORM\ManyToOne(targetEntity="ServiceDuration")
+	 */
+	protected $duration;
+
+	/**
+	 * @var float
+	 * @ORM\Column(type="float", nullable=true)
+	 */
+	protected $priceDefault;
+
+	/**
+	 * @var float
+	 * @ORM\Column(type="float", nullable=true)
+	 */
+	protected $priceCurrent;
+
+	/**
+	 * @var \Entity\Currency
+	 * @ORM\ManyToOne(targetEntity="Entity\Currency")
+	 */
+	protected $currency;
+
+	/**
+	 * @var Company
+	 * @ORM\ManyToOne(targetEntity="Company")
+	 */
+	protected $company;
+
+	/**
+	 * @var \Entity\Location\Location
+	 * @ORM\ManyToOne(targetEntity="Entity\Location\Location")
+	 */
+	protected $primaryLocation;
+
 }
