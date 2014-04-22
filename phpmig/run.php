ea3780f0d6f8aa19514e7a0b63c5a2112c0b433f
achtan <durika.d@gmail.com>
2014-04-22 08:46:07 +0200

    phpmig alpha

--- phpmig/run.php
+++ phpmig/run.php
@@ -14,9 +14,10 @@ extension_loaded('mbstring') && mb_internal_encoding('UTF-8');
 
 
 $command = isset($argv[1]) ? $argv[1] : 'migrate';
+$option = isset($argv[2]) ? $argv[2] : NULL;
+$optionValue = isset($argv[3]) ? $argv[3] : NULL;
 
 
-$command = VENDOR_DIR . '/bin/phpmig ' . $command . ' -b '. CONFIG_DIR . '/bootstrap.php';
+$command = VENDOR_DIR . "/bin/phpmig {$command} {$option} {$optionValue} -b ". CONFIG_DIR . '/bootstrap.php';
 echo $command . "\n";
 system($command);
-//exec($command);
