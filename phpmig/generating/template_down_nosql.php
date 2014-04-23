fc98ac89741da29c1a5edc75fbcd18f57c2ab7f2
achtan <durika.d@gmail.com>
2014-04-23 09:56:10 +0200

    save

--- phpmig/generating/template_down_nosql.php
+++ phpmig/generating/template_down_nosql.php
@@ -4,15 +4,10 @@
 class MigrationName extends \Migration\Migration
 {
 
-	/** @var \SystemContainer */
-	private $appContainer;
-
-
 
 	public function down()
 	{
-		$this->appContainer = $this->getContainer()['appContainer'];
-		$this->connection = $this->appContainer->notORMConnection;
+
 	}
 
 
--- phpmig/generating/template_up_nosql.php
+++ phpmig/generating/template_up_nosql.php
@@ -4,14 +4,10 @@
 class MigrationName extends \Migration\Migration
 {
 
-	/** @var \SystemContainer */
-	private $appContainer;
-
-
 
 	public function up()
 	{
-		$this->appContainer = $this->getContainer()['appContainer'];
+
 	}
 
 
