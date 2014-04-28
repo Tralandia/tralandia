9512206a9ba7f1cabd0dccb2261cc92e4202249e
achtan <durika.d@gmail.com>
2014-04-28 08:32:09 +0200

    Invalidate template cache command

--- libs/Tralandia/Console/BaseCommand.php
+++ libs/Tralandia/Console/BaseCommand.php
@@ -13,4 +13,20 @@ use Symfony\Component\Console\Command\Command;
 
 class BaseCommand extends Command {
 
+
+	public function report($message)
+	{
+		$print = $message;
+		if(is_array($message)) {
+			$print = implode("\n", $message);
+		}
+		echo $print . "\n";
+
+		$log = $message;
+		if(is_array($message)) {
+			$log = implode(" | ", $message);
+		}
+		Nette\Diagnostics\Debugger::log($log, 'command-report');
+	}
+
 }
