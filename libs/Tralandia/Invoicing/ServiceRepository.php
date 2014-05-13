84dad19a05bd8e9bc8781bee3dfe8e9ebfe38375
achtan <durika.d@gmail.com>
2014-05-13 08:03:46 +0200

    save

---
+++ libs/Tralandia/Invoicing/ServiceRepository.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tralandia\Invoicing;
+
+
+use Nette;
+use Tralandia\Lean\BaseRepository;
+
+/**
+ * Class UnitRepository
+ * @package Tralandia\Rental
+ *
+ * @method save(\Tralandia\Invoicing\Service $entity)
+ * @method \Tralandia\Invoicing\Service createNew()
+ * @method \Tralandia\Invoicing\Service find()
+ * @method \Tralandia\Invoicing\Service findOneBy()
+ * @method \Tralandia\Invoicing\Service[] findBy()
+ * @method \Tralandia\Invoicing\Service[] findAll()
+ */
+class ServiceRepository extends BaseRepository
+{
+
+
+}
