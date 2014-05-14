a5ee97b244b473d524a2f3b45841d3725847f581
achtan <durika.d@gmail.com>
2014-05-14 15:04:44 +0200

    save

--- libs/Tralandia/User/User.php
+++ libs/Tralandia/User/User.php
@@ -7,8 +7,11 @@ use Nette;
 
 /**
  * @property string $login
+ * @property array|null $invoicingInformation m:passThru(jsonIn|jsonOut)
  */
 class User extends \Tralandia\Lean\BaseEntity
 {
+
+
 
 }
