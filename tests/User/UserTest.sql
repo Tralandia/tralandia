e3d3245088d72d846530feee17b332f1fe4a3a14
achtan <durika.d@gmail.com>
2014-05-15 12:27:10 +0200

    save

--- tests/User/UserTest.sql
+++ tests/User/UserTest.sql
@@ -6,4 +6,4 @@ VALUES
 
 INSERT INTO `user` (`id`, `role_id`, `language_id`, `login`, `password`, `newsletter`, `details`, `oldId`, `created`, `updated`, `primaryLocation_id`, `invoicingInformation`)
 VALUES
-	(1, NULL, 38, 'informacio@hotelstabarbara.com', '2a43a259eea6aeee27ecf8c895a29ee8b91a5548', 1, 'null', 10199, '2013-06-16 21:35:21', '2013-06-16 21:35:21', NULL, '{\"rule\":\"($n != 1)\",\"names\":[\"Singular\",\"0, Plural\"]}');
+	(1, NULL, 38, 'informacio@hotelstabarbara.com', '2a43a259eea6aeee27ecf8c895a29ee8b91a5548', 1, 'null', 10199, '2013-06-16 21:35:21', '2013-06-16 21:35:21', NULL, NULL);
