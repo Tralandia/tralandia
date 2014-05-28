3038aab9d1fe6cdc228274c03f699787b0dd28f2
Jan Czibula <jan@czibula.com>
2014-05-28 09:33:17 +0200

    save

---
+++ app/FrontModule/presenters/AttractionListPresenter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FrontModule;
+
+use FrontModule\Forms\Rental\IReservationFormFactory;
+use Nette\Utils\Html;
+use Nette\Utils\Strings;
+
+class AttractionListPresenter extends BasePresenter {
+
+	public function actionDefault()
+	{
+		
+	}
+
+}
