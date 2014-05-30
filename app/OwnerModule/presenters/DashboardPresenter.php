3e8439180e6d60688de5286fef0c96bab4a1578f
achtan <durika.d@gmail.com>
2014-05-30 08:16:18 +0200

    save

--- app/OwnerModule/presenters/DashboardPresenter.php
+++ app/OwnerModule/presenters/DashboardPresenter.php
@@ -4,9 +4,10 @@ namespace OwnerModule;
 
 
 class DashboardPresenter extends BasePresenter {
-	
+
 	public function actionDefault() {
 		$this->template->steps = \OwnerModule\RentalEditPresenter::getSteps();
+		$this->template->rentalList = $this->loggedUser->getRentals();
 	}
 
 }
---
+++ phpmig/migrations/20140530080353_invoicing_new_services/20140530080353_invoicing_new_services_up.sql
@@ -0,0 +1,4 @@
+-- InvoicingNewServices migration UP file
+
+INSERT INTO `invoicing_service` (`id`, `type_id`, `duration_id`, `currency_id`, `company_id`, `priceDefault`, `priceCurrent`, `oldId`, `created`, `updated`, `primaryLocation_id`)
+VALUES (NULL, '1', '5', '1', NULL, '159', '99', NULL, '2014-01-01', '2014-01-01', NULL);
