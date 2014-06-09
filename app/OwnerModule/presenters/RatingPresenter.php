d7aeeefea12c5c9f8a60b99f3efc72c6d3cd43b1
achtan <durika.d@gmail.com>
2014-06-09 11:13:05 +0200

    Rental review v1

--- app/OwnerModule/presenters/RatingPresenter.php
+++ app/OwnerModule/presenters/RatingPresenter.php
@@ -3,16 +3,74 @@
 namespace OwnerModule;
 
 
+use Nette\Application\UI\Multiplier;
+
 class RatingPresenter extends BasePresenter {
 
-	public function actionDefault($id = NULL)
-	{
-		$this->template->rentals = $this->loggedUser->getRentals();
+	/**
+	 * @autowire
+	 * @var \Tralandia\RentalReview\RentalReviewRepository
+	 */
+	protected $rentalReviewRepository;
 
-		if ($id) {
-			// vyfiltrovat hodnotenia pre konkretny objekt
-			$this->template->thisRental = $this->rentalDao->find($id);
-		}
+	/**
+	 * @autowire
+	 * @var \Tralandia\SearchCache\InvalidateRentalListener
+	 */
+	protected $invalidateRentalListener;
+
+
+	protected $rental;
+
+
+	public function actionDefault($id)
+	{
+		$this->rental = $this->findRental($id);
+		$this->checkPermission($this->rental, 'edit');
+
+
+		$this->template->thisRental = $this->rental;
+		$this->template->rentals = $this->loggedUser->getRentals();
+		$this->template->avgRating = $this->rentalReviewRepository->getRentalAvgRate($this->rental);
+	}
+
+	public function renderDefault($id)
+	{
+		$this->template->reviews = $this->em->getRepository(RENTAL_REVIEW_ENTITY)->findBy(['rental' => $this->rental], ['created' => 'DESC']);
+	}
+
+
+	protected function createComponentAnswerForm()
+	{
+		$factory = function($reviewId) {
+			$form = $this->simpleFormFactory->create();
+			$form->getElementPrototype()->addClass('ajax');
+
+			$form->addTextArea('answer');
+			$form->addHidden('reviewId', $reviewId);
+			$form->addSubmit('submit', 'odpovedat');
+
+			$form->onSuccess[] = $this->processAnswerForm;
+
+			return $form;
+		};
+
+		return new Multiplier($factory);
+	}
+
+	public function processAnswerForm($form)
+	{
+		$values = $form->getValues();
+
+		$review = $this->em->getRepository(RENTAL_REVIEW_ENTITY)->find($values->reviewId);
+		$this->checkPermission($review->rental, 'edit');
+
+		$review->ownerAnswer = $values->answer;
+
+		$this->em->flush($review);
+
+		$this->invalidateRentalListener->onSuccess($review->rental);
+		$this->invalidateControl('reviews');
 	}
 
 }
