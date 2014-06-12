142415331360275f0f00918d6d0b496e969b048f
achtan <durika.d@gmail.com>
2014-06-12 09:08:27 +0200

    save

--- phpmig/migrations/20140611130911_ps_landing_page/20140611130911_ps_landing_page_down.sql
+++ phpmig/migrations/20140611130911_ps_landing_page/20140611130911_ps_landing_page_down.sql
@@ -1,1 +1,6 @@
 -- PsLandingPage migration DOWN file
+
+
+ALTER TABLE page ADD genericContent_id INT DEFAULT NULL, CHANGE parameters parameters longtext DEFAULT NULL;
+ALTER TABLE page ADD CONSTRAINT FK_140AB620F0A1919F FOREIGN KEY (genericContent_id) REFERENCES phrase (id);
+CREATE UNIQUE INDEX UNIQ_140AB620F0A1919F ON page (genericContent_id);
